52f1fbe185fc61cca338e4e000f4a41c64f431b5
Vlad Mocanu <vladmoca90@gmail.com>
2019-01-06 05:27:29 +0000

    Deleted files

--- scripts/name.js
+++ scripts/name.js
@@ -2,19 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-	// constructor method begins here:
+  // constructor method begins here:
   constructor(props) {
     super(props);
     this.state = { title: 'Best App' };
   }
-	
+
   render() {
-    return (
-      <h1>
-       {this.state.title}
-      </h1>
+    return React.createElement(
+      'h1',
+      null,
+      this.state.title
     );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(React.createElement(App, null), document.getElementById('app'));
