bce9be2ac5cec5077151c3af2fbb4ea6e99d9d00
Vlad Mocanu <vladmoca90@gmail.com>
2019-01-06 12:39:28 +0000

    Works

---
+++ scripts/react/name.js
@@ -0,0 +1,10 @@
+class MyComponentClass extends React.Component {
+  render() {
+    return <h1>Vlad Mocanu</h1>;
+  }
+};
+
+ReactDOM.render(
+  <MyComponentClass />,
+  document.getElementById('app')
+);
