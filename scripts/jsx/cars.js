cd74df9fd4cc8701225725810e6323ee2322d0e1
Vlad Mocanu <vladmoca90@gmail.com>
2019-01-31 22:33:52 +0000

    Created a sample object

---
+++ scripts/jsx/cars.js
@@ -0,0 +1,32 @@
+const data = [
+    {
+        make: 'Honda',
+        model: 'Civic',
+        year: 2009,
+        img: '',
+    },
+    {
+        make: 'Honda',
+        model: 'Civic',
+        year: 2009,
+        img: '',
+    },
+    {
+        make: 'Honda',
+        model: 'Civic',
+        year: 2009,
+        img: '',
+    },
+    {
+        make: 'Honda',
+        model: 'Civic',
+        year: 2009,
+        img: '',
+    },
+];
+
+class Showroom extends React.Component {
+
+}
+
+ReactDOM.render(<Showroom />, document.getElementById('app'));
