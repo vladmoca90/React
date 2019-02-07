90cb55a9d84d2f5083507ddddfdd22a48df64dad
Vlad Mocanu <vladmoca90@gmail.com>
2019-02-07 22:27:27 +0000

    Added .babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
             exclude: /node_modules/,
             loader: 'babel-loader',
             query: {
-               presets: ["@babel/env", "@babel/react"]
+               presets: ["babel-preset-env", "react", "babel-preset-es2015"]
             }
          }
       ]
