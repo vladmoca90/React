3f833eedf88b5c5c8119b92c8dcd73ab0a0ab354
Vlad Mocanu <vladmoca90@gmail.com>
2019-02-16 22:49:08 +0000

    Created the html structure

--- src/cars.js
+++ src/cars.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import cars from './data';
+
+const carName = cars.map(cars => <span>{cars.name}</span>);
+const carYear = cars.map(cars => <span>{cars.year}</span>);
+const carPrice = cars.map(cars => <span>{cars.price}</span>);
+const carImg = cars.map(cars => <span>{cars.image}</span>);
 
 class Showroom extends Component {
   render() {
     return (
-      <div>
-        
+      <div class="card">
+        <div class="card__header">
+          {carName} {carYear}
+          <div>£{carPrice}</div>
+        </div>
+        <div class="card__body">
+          <img alt={carName} src={carImg} />
+        </div>
+        <div class="card__buttons">
+
+        </div>
       </div>
     );
   }
---
+++ src/data.js
@@ -0,0 +1,34 @@
+const cars = [
+    {
+      name: "Ford Focus 1.8 Style 5dr",
+      year: 2010,
+      price: 1950,
+      image: '../public/images/ford-focus.jpg',
+    },
+     {
+      name: "Ford Focus 1.8 Style 5dr",
+      year: 2010,
+      price: 1950,
+      image: '../public/images/ford-focus.jpg',
+    },
+     {
+      name: "Ford Focus 1.8 Style 5dr",
+      year: 2010,
+      price: 1950,
+      image: '../public/images/ford-focus.jpg',
+    },
+     {
+      name: "Ford Focus 1.8 Style 5dr",
+      year: 2010,
+      price: 1950,
+      image: '../public/images/ford-focus.jpg',
+    },
+     {
+      name: "Ford Focus 1.8 Style 5dr",
+      year: 2010,
+      price: 1950,
+      image: '../public/images/ford-focus.jpg',
+    },
+];
+
+export default cars;
