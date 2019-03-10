63f4d6d15f90a3360accbb6d57e54711c2d6788a
Vlad Mocanu <vladmoca90@gmail.com>
2019-03-10 20:13:51 +0000

    Remade everything

--- src/data.js
+++ src/data.js
@@ -17,6 +17,24 @@ const cars = [
       price: 2495,
       image: '../images/subaru-impreza.jpg',
     },
+    {
+      name: 'Ford Focus 1.6 ZETEC S TDCI 5d 113 BHP 5-Door',
+      year: 2014,
+      price: 8590,
+      image: '../images/ford-focus.jpg',
+    },
+    {
+      name: 'Alfa Romeo Giulieta 2.0 JTDM Exclusive 5-Door',
+      year: 2014,
+      price: 7995,
+      image: '../images/alfa-romeo.jpg',
+    },
+    {
+      name: 'Subaru Impreza 2.0 Turbo WRX 5dr',
+      year: 2001,
+      price: 2495,
+      image: '../images/subaru-impreza.jpg',
+    },
 ];
 
 export default cars;
