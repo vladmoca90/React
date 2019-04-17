acb4fddc314ff863c5514fc1b55d797c7d681166
Vlad Mocanu <vladmoca90@gmail.com>
2019-04-17 23:43:23 +0300

    Adjusted the header

--- src/data.js
+++ src/data.js
@@ -35,42 +35,6 @@ const cars = [
       price: 2495,
       image: '../images/subaru-impreza.jpg',
     },
-    {
-      name: 'Ford Focus 1.6 ZETEC S TDCI 5d 113 BHP 5-Door',
-      year: 2014,
-      price: 8590,
-      image: '../images/ford-focus.jpg',
-    },
-    {
-      name: 'Alfa Romeo Giulieta 2.0 JTDM Exclusive 5-Door',
-      year: 2014,
-      price: 7995,
-      image: '../images/alfa-romeo.jpg',
-    },
-    {
-      name: 'Subaru Impreza 2.0 Turbo WRX 5dr',
-      year: 2001,
-      price: 2495,
-      image: '../images/subaru-impreza.jpg',
-    },
-    {
-      name: 'Renault Megane 1.5 dCi FAP Dynamique Tom Tom Coupe 3dr Diesel EDC',
-      year: 2011,
-      price: 5290,
-      image: '../images/subaru-impreza.jpg',
-    },
-    {
-      name: 'Volkswagen Golf R Dsg 5dr Auto',
-      year: 2016,
-      price: 21995,
-      image: '../images/subaru-impreza.jpg',
-    },
-    {
-      name: 'Subaru Impreza 2.0 Turbo WRX 5dr',
-      year: 2001,
-      price: 2495,
-      image: '../images/subaru-impreza.jpg',
-    },
 ];
 
 export default cars;
