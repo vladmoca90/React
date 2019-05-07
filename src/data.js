a3bd85424bc462a1989463a5472fad8bd5ef64c7
Vlad Mocanu <vladmoca90@gmail.com>
2019-05-07 22:28:29 +0100

    Added ids in teh data.js

--- src/data.js
+++ src/data.js
@@ -4,36 +4,42 @@ const cars = [
       year: 2014,
       price: 8590,
       image: '../images/ford-focus.jpg',
+      id: 1,
     },
     {
       name: 'Alfa Romeo Giulieta 2.0 JTDM Exclusive 5-Door',
       year: 2014,
       price: 7995,
       image: '../images/alfa-romeo.jpg',
+      id: 2,
     },
     {
       name: 'Subaru Impreza 2.0 Turbo WRX 5dr',
       year: 2001,
       price: 2495,
       image: '../images/subaru-impreza.jpg',
+      id: 3,
     },
     {
       name: 'Renault Megane 1.5 dCi FAP Dynamique Tom Tom Coupe 3dr Diesel EDC',
       year: 2011,
       price: 5290,
       image: '../images/subaru-impreza.jpg',
+      id: 4,
     },
     {
       name: 'Volkswagen Golf R Dsg 5dr Auto',
       year: 2016,
       price: 21995,
       image: '../images/subaru-impreza.jpg',
+      id: 5,
     },
     {
       name: 'Subaru Impreza 2.0 Turbo WRX 5dr',
       year: 2001,
       price: 2495,
       image: '../images/subaru-impreza.jpg',
+      id: 6,
     },
 ];
 
