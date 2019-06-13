b482facffa039ea0b82524af033221d48e1f3289
Vlad Mocanu <vladmoca90@gmail.com>
2019-06-13 23:41:33 +0100

    Deleted the all data

--- src/filter.js
+++ src/filter.js
@@ -1,34 +1,11 @@
 import React from 'react';
-import './cars.css';
-import cars from './data';
+import './filter.css';
+import Filter from './data';
 
-const total = <span className="total-number">{cars.length}</span>;
-const allCars =  cars.map((car, i) =>
-        <div className="card" key={i}>
-          <div className="card__header">
-            <div className="card__name">{car.name} {car.year}</div>
-            <div className="card__price">&pound;{car.price}</div>
-          </div>
-          <div className="card__body">
-            <img className="card__image" alt={car.name} src={car.image} />
-          </div>
-          <div className="card__buttons">
-            <a className="card__link" href="##">Enquiry</a>
-            <a className="card__link" href="##">Brochure</a>
-            <a className="card__link" href="##">More details</a>
-          </div>
-        </div>
-      );
-
-class Showroom extends React.Component {
+class Filter extends React.Component {
   render() {
-    return (
-    <div className="main">
-      <div className="showroom-container">{allCars}</div>
-      <div className="total-cars">There are {total} cars available</div>
-    </div>
-    );
+    return ;
   }
 }
 
-export default Showroom;
+export default Filter;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Showroom from './cars';
+import Filter from './filter';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Showroom />, document.getElementById('root'));
+ReactDOM.render(<Filter />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
