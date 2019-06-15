589392cb7b0663731d1bd68c9cfa048c200597e7
Vlad Mocanu <vladmoca90@gmail.com>
2019-06-16 00:00:58 +0100

    Rewrote the code

--- src/filter.js
+++ src/filter.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './filter.css';
 import Filter from './data';
 
+var filter = <div class="filter-box">
+        <input class="filter-field" type="text" />
+      </div>;
+
 class Filter extends React.Component {
   render() {
-    return (
-      <div class="filter-box">
-        <input type="text"/>
-      </div>
-  }
+    return filter;
 }
 
 export default Filter;
