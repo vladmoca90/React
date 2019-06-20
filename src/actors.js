f3e659aca19aca64e4ec20d4086635df6ea68318
Vlad Mocanu <vladmoca90@gmail.com>
2019-06-20 21:22:31 +0100

    Created the HTML

--- src/actors.js
+++ src/actors.js
@@ -2,14 +2,27 @@ import React from 'react';
 import './filter.css';
 import actors from './data';
 
-var actorsList = <div className="filter-box">
-        <input className="filter-field" type="text" />
-        { actors }
-      </div>;
+var allActors = actors.map((actor) =>
+            <article className="article">
+                <div className="article__content">
+                    <h2 role="heading" className="article__heading">{ actor.name }</h2>
+                    <p role="heading" className="article__heading-sub">{ actor.dateOfBirth }</p>
+                    <p role="heading" className="article__heading-sub">{ actor.placeOfBirth }</p>
+                    <p role="text" className="article__text">{ actor.biography }</p>
+                </div>
+                <div className="article__image">
+                    <img src="{actor.image}" />
+                </div>
+            </article>);
 
 class Actors extends React.Component {
   render() {
-    return actorsList;
+    return (
+      <section className="box">
+        <input type="text" className="box__filter" />
+        {allActors}
+      </section>
+    );
 }
 
 export default Actors;
