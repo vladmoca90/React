f743cf1d26cb689d209329d1f9763df49c40c936
Vlad Mocanu <vladmoca90@gmail.com>
2019-06-23 23:01:57 +0100

    Deleted the role=" " attributes

--- src/actors.js
+++ src/actors.js
@@ -5,10 +5,10 @@ import actors from './data';
 var allActors = actors.map((actor) =>
             <article className="article">
                 <div className="article__content">
-                    <h2 role="heading" className="article__heading">{ actor.name }</h2>
-                    <p role="heading" className="article__heading-sub">{ actor.dateOfBirth }</p>
-                    <p role="heading" className="article__heading-sub">{ actor.placeOfBirth }</p>
-                    <p role="text" className="article__text">{ actor.biography }</p>
+                    <h2 className="article__heading">{ actor.name }</h2>
+                    <p className="article__heading-sub">{ actor.dateOfBirth }</p>
+                    <p className="article__heading-sub">{ actor.placeOfBirth }</p>
+                    <p className="article__text">{ actor.biography }</p>
                 </div>
                 <div className="article__image">
                     <img alt={actor.name} src={actor.image} />
