f1607a4ab14a69c12c993c3174741ebd750a289d
Vlad Mocanu <vladmoca90@gmail.com>
2019-08-03 22:34:57 +0100

    Deleted the code

--- src/actors.js
+++ src/actors.js
@@ -2,28 +2,3 @@ import React from 'react';
 import './actors.css';
 import actors from './data';
 
-var allActors = actors.map((actor) =>
-            <article className="article">
-                <div className="article__content">
-                    <h2 className="article__heading">{ actor.name }</h2>
-                    <p className="article__heading-sub">{ actor.dateOfBirth }</p>
-                    <p className="article__heading-sub">{ actor.placeOfBirth }</p>
-                    <p className="article__text">{ actor.biography }</p>
-                </div>
-                <div className="article__image">
-                    <img alt={actor.name} src={actor.image} />
-                </div>
-            </article>);
-
-class Actors extends React.Component {
-  render() {
-    return (
-      <section className="box">
-        <input type="text" className="box__filter" />
-        {allActors}
-      </section>
-    );
-  }
-}
-
-export default Actors;
