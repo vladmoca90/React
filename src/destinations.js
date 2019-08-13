a15f0f68f375ed8625b04985eda023546bded3e3
Vlad Mocanu <vladmoca90@gmail.com>
2019-08-13 21:42:12 +0100

    Added the utilities and variables scss files

--- src/destinations.js
+++ src/destinations.js
@@ -3,9 +3,26 @@ import './destination.css';
 import destinations from './data';
 
 var allDestinations = destinations.map((destination) =>
-            <article className="article">
-                
-            </article>);
+             <div class="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+                    <div class="card u-bg--white u-border">
+                        <div class="u-relative">
+                            <img src="../../../images/cards/destinations/Malaysia.jpg" alt="Malaysia travel" class="card__img" />
+                        </div>
+                        <div class="card__content u-relative">
+                            <p class="card__heading">Malaysia</p>
+                            <p class="card__subHeading u-text--bold">Asia</p>
+                            <p class="card__text">Malaysia is a Southeast Asian country occupying parts of the Malay Peninsula and the island of
+                                Borneo. Its known for its beaches, rainforests and mix of Malay, Chinese, Indian and European
+                                cultural influences. The capital, Kuala Lumpur, is home to colonial buildings, busy shopping
+                                districts such as Bukit Bintang and skyscrapers such as the iconic, 451m-tall Petronas Twin
+                                Towers.
+                            </p>
+                            <div class="card__link-container u--absolute">
+                                <a href="##" class="card__link u-relative">See our offers</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>);
 
 class Destinations extends React.Component {
   render() {
