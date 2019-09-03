b4077de52356b7df72f2ca60021908bfc624cc8b
Vlad Mocanu <vladmoca90@gmail.com>
2019-09-03 15:05:21 +0300

    Deleted the .test.js

--- src/projects/cars/cars.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Showroom from './cars';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Showroom />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
