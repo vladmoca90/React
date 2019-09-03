c58bd003c5371cf476cceeecaad345d3ea891dd1
Vlad Mocanu <vladmoca90@gmail.com>
2019-09-04 02:57:51 +0300

    The 'cars' project works

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Showroom from './cars';
-import * as serviceWorker from './serviceWorker';
+import Destinations from './destinations';
+import * as serviceWorker from '../../serviceWorker';
 
-ReactDOM.render(<Showroom />, document.getElementById('root'));
+ReactDOM.render(<Destinations />, document.getElementById('root'));
 
 serviceWorker.unregister();
