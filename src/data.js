1d603c07e663ff4a7eff9d4a023c3e3c81ff18ff
Vlad Mocanu <vladmoca90@gmail.com>
2020-01-19 21:43:06 +0000

    Created the data for the code

--- src/data.js
+++ src/data.js
@@ -1,6 +1,31 @@
 const airline = [
     {
-
+        name: 'Air Algerie',
+        destinations: 'Algiers',
+    },
+    {
+        name: 'Cathay Pacific',
+        destinations: 'Hong Kong',
+    },
+    {
+        name: 'El Al',
+        destinations: 'Tel Aviv-Ben Gurion',
+    },
+    {
+        name: 'Japan Airlines',
+        destinations: 'Tokyo-Haneda',
+    },
+    {
+        name: 'Oman Air',
+        destinations: 'Muscat',
+    },
+    {
+        name: 'Turkish Airlines',
+        destinations: 'Istanbul',
+    },
+    {
+        name: 'Virgin Atlantic',
+        destinations: '	Atlanta, Boston, Delhi, Havana, Hong Kong, Johannesburg, Lagos, Las Vegas, Los Angeles, Miami, Mumbai, New York–JFK, Newark, San Francisco, São Paulo–Guarulhos, Seattle/Tacoma, Shanghai–Pudong, Tel Aviv, Washington–Dulles',
     }
 ];
 
