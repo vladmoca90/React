d4cb057b910cb9d1234e79a0790568be0c11e09a
Vlad Mocanu <vladmoca90@gmail.com>
2020-02-20 19:10:53 +0000

    Changed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Destinations from './airlines-destinations';
-import * as serviceWorker from '../../serviceWorker';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Destinations />, document.getElementById('root'));
 
