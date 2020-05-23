8035e6e79ab5076058dfb12bfdcb0e795ab4b657
Vlad Mocanu <vladmoca90@gmail.com>
2020-05-23 16:03:45 +0100

    Corrected some data. Wrote " instead of '

--- src/data.js
+++ src/data.js
@@ -1,71 +1,71 @@
 const flights = [
     {
-        airline: 'Air Algerie',
-        destinations: 'Algiers',
+        airline: "Air Algerie",
+        destinations: "Algiers",
     },
      {
-        airline: 'Air France',
-        destinations: 'Paris-Charles de Gaulle',
+        airline: "Air France",
+        destinations: "Paris-Charles de Gaulle",
     },
     {
-        airline: 'Avianca',
-        destinations: 'Bogota-El Dorado',
+        airline: "Avianca",
+        destinations: "Bogota-El Dorado",
     },
     {
-        airline: 'Brussels Airlines',
-        destinations: 'Brussels-International',
+        airline: "Brussels Airlines",
+        destinations: "Brussels-International",
     },
     {
-        airline: 'Cathay Pacific',
-        destinations: 'Hong Kong',
+        airline: "Cathay Pacific",
+        destinations: "Hong Kong",
     },
     {
-        airline: 'El Al',
-        destinations: 'Tel Aviv-Ben Gurion',
+        airline: "El Al",
+        destinations: "Tel Aviv-Ben Gurion",
     },
      {
-        airline: 'Hainan Airlines',
-        destinations: ['Beijing-Capital', 'Shanghai-Pudong'],
+        airline: "Hainan Airlines",
+        destinations: ["Beijing-Capital", "Shanghai-Pudong"],
     },
     {
-        airline: 'Iran Air',
-        destinations: 'Teheran-Khomeini',
+        airline: "Iran Air",
+        destinations: "Tehran-Khomeini",
     },
     {
-        airline: 'Japan Airlines',
-        destinations: 'Tokyo-Haneda',
+        airline: "Japan Airlines",
+        destinations: "Tokyo-Haneda",
     },
     {
-        airline: 'KLM',
-        destinations: 'Amsterdam',
+        airline: "KLM",
+        destinations: "Amsterdam",
     },
     {
-        airline: 'Lufthansa',
-        destinations:  ['Frankfurt', 'Munich'],
+        airline: "Lufthansa",
+        destinations:  ["Frankfurt", "Munich"],
     },
     {
-        airline: 'Mexicana',
-        destinations: 'Mexico City',
+        airline: "Mexicana",
+        destinations: "Mexico City",
     },
     {
-        airline: 'Royal Air Jordanian',
-        destinations: 'Amman-Queen Alia',
+        airline: "Royal Air Jordanian",
+        destinations: "Amman-Queen Alia",
     },
     {
-        airline: 'Oman Air',
-        destinations: 'Muscat',
+        airline: "Oman Air",
+        destinations: "Muscat",
     },
     {
-        airline: 'Turkish Airlines',
-        destinations: 'Istanbul-Ataturk',
+        airline: "Turkish Airlines",
+        destinations: "Istanbul-Ataturk",
     },
      {
-        airline: 'United Airlines',
-        destinations: ['Chicago', 'Dallas-Fort Worth', 'Houston-Intercontinental', 'Los Angeles', 'Newark', 'San Francisco'],
+        airline: "United Airlines",
+        destinations: ["Chicago-O'Hare", "Dallas-Fort Worth", "Houston-Intercontinental", "Los Angeles", "Newark", "San Francisco"],
     },
     {
-        airline: 'Virgin Atlantic',
-        destinations: ['Atlanta', 'Boston', 'Delhi', 'Havana', 'Hong Kong', 'Johannesburg', 'Lagos', 'Las Vegas', 'Los Angeles', 'Miami', 'Mumbai', 'New York–JFK', 'Newark', 'San Francisco', 'São Paulo–Guarulhos', 'Seattle/Tacoma', 'Shanghai–Pudong', 'Tel Aviv', 'Washington–Dulles'],
+        airline: "Virgin Atlantic",
+        destinations: ["Atlanta", "Boston", "Delhi", "Havana", "Hong Kong", "Johannesburg", "Lagos", "Las Vegas", "Los Angeles", "Miami", "Mumbai", "New York–JFK", "Newark", "San Francisco", "São Paulo–Guarulhos", "Seattle/Tacoma", "Shanghai–Pudong", "Tel Aviv-Ben Gurion", "Washington–Dulles"],
     }
 ];
 
