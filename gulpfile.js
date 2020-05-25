bf907244805c4bb0e97cea2a086f814f1c7cc88d
Vlad Mocanu <vladmoca90@gmail.com>
2020-05-25 20:46:46 +0100

    Created a gulp file

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('src/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('src/'))
+});
+
+//Watch task
+gulp.task('default', function () {
+  return gulp.watch('src/**/*.scss', gulp.series('sass'));
+});
