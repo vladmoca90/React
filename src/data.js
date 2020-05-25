6ada974fb5c376e12ed1dbb3191d815af1ef3fa4
Vlad Mocanu <vladmoca90@gmail.com>
2020-05-25 15:50:18 +0100

    Trying to fix the images issue

--- src/data.js
+++ src/data.js
@@ -3,37 +3,37 @@ const cars = [
       name: 'Ford Focus 1.6 ZETEC S TDCI 5d 113 BHP 5-Door',
       year: 2014,
       price: 8590,
-      image: '../../images/cars/ford-focus.jpg',
+      image: '../images/cars/ford-focus.jpg',
     },
     {
       name: 'Alfa Romeo Giulieta 2.0 JTDM Exclusive 5-Door',
       year: 2014,
       price: 7995,
-      image: '../../images/cars/alfa-romeo.jpg',
+      image: '../images/cars/alfa-romeo.jpg',
     },
     {
       name: 'Subaru Impreza 2.0 Turbo WRX 5dr',
       year: 2001,
       price: 2495,
-      image: '../../images/cars/subaru-impreza.jpg',
+      image: '../images/cars/subaru-impreza.jpg',
     },
     {
       name: 'Renault Megane 1.5 dCi FAP Dynamique Tom Tom Coupe 3dr Diesel EDC',
       year: 2011,
       price: 5290,
-      image: '../../images/cars/subaru-impreza.jpg',
+      image: '../images/cars/subaru-impreza.jpg',
     },
     {
       name: 'Volkswagen Golf R Dsg 5dr Auto',
       year: 2016,
       price: 21995,
-      image: '../../images/cars/subaru-impreza.jpg',
+      image: '../images/cars/subaru-impreza.jpg',
     },
     {
       name: 'Subaru Impreza 2.0 Turbo WRX 5dr',
       year: 2001,
       price: 2495,
-      image: '../../images/cars/subaru-impreza.jpg',
+      image: '../images/cars/subaru-impreza.jpg',
     },
 ];
 
