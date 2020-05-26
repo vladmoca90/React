0eddceb0b22976d54df34af8545d3cad59d2c2f8
Vlad Mocanu <vladmoca90@gmail.com>
2020-05-26 23:08:47 +0100

    Moved files back

--- src/projects/cars/index.js
+++ src/projects/cars/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Showroom from './cars';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from '../../serviceWorker';
 
 ReactDOM.render(<Showroom />, document.getElementById('root'));
 
