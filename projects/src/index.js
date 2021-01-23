cf75d671f82c2781b4f9e10784ee10099cd92c43
Vlad Mocanu <vladmoca90@gmail.com>
2021-01-23 10:20:11 +0000

    The projects work now but not the styling

--- projects/src/index.js
+++ projects/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Actors from './actors';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Actors />, document.getElementById('root'));
 
-serviceWorker.unregister();
+// serviceWorker.unregister();
