b62da6a855e3079d7ea2f737a40d26fa2a38c4df
Vlad Mocanu <vladmoca90@gmail.com>
2021-01-23 10:16:50 +0000

    Testing again

--- projects/src/projects/actors/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Actors from '.';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<Actors />, document.getElementById('root'));
-
-serviceWorker.unregister();
