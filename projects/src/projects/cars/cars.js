e70ce82379966eacd1b40b56a5a12fa3a6a7c6c8
Vlad Mocanu <vladmoca90@gmail.com>
2021-03-13 22:42:09 +0000

    Deleted spaces

--- projects/src/projects/cars/cars.js
+++ projects/src/projects/cars/cars.js
@@ -4,28 +4,28 @@ import cars from './data';
 
 const total = <span className="total-number">{cars.length}</span>;
 const allCars =  cars.map((car) =>
-        <div className="card">
-          <div className="card__header">
-            <div className="card__name" key={car.name}>{car.name} {car.year}</div>
-            <div className="card__price" key={car.price}>&pound;{car.price}</div>
-          </div>
-          <div className="card__body">
-            <img className="card__image" ky={car.name} alt={car.name} src={car.image} />
-          </div>
-          <div className="card__buttons">
-            <a className="card__link" href="##">Enquiry</a>
-            <a className="card__link" href="##">Brochure</a>
-            <a className="card__link" href="##">More details</a>
-          </div>
-        </div>
-      );
+  <div className="card">
+    <div className="card__header">
+      <div className="card__name" key={car.name}>{car.name} {car.year}</div>
+      <div className="card__price" key={car.price}>&pound;{car.price}</div>
+    </div>
+    <div className="card__body">
+      <img className="card__image" ky={car.name} alt={car.name} src={car.image} />
+    </div>
+    <div className="card__buttons">
+      <a className="card__link" href="##">Enquiry</a>
+      <a className="card__link" href="##">Brochure</a>
+      <a className="card__link" href="##">More details</a>
+    </div>
+  </div>
+);
 
 class Showroom extends React.Component {
   constructor() {
-      super();
-      this.state = {
-        placeholder:'Search...'
-      };
+    super();
+    this.state = {
+      placeholder:'Search...'
+    };
   }
   render() {
     return (
