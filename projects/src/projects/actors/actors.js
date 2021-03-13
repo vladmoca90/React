8458ad2ef9541316252fe1d04f42a9210c06443e
Vlad Mocanu <vladmoca90@gmail.com>
2021-03-13 22:43:17 +0000

    Deleted spaces

--- projects/src/projects/actors/actors.js
+++ projects/src/projects/actors/actors.js
@@ -3,31 +3,31 @@ import './actors.css';
 import actors from './data';
 
 const allActors = actors.map((actor) =>
-    <div className="article" key={ actor.name }>
-        <div className="article__content">
-            <h2 className="article__heading">{ actor.name }</h2>
-            <p className="article__heading-sub">{ actor.dateOfBirth }</p>
-            <p className="article__heading-sub">{ actor.placeOfBirth }</p>
-            <p className="article__text">{ actor.biography }</p>
-        </div>
-        <div className="article__image">
-            <img alt={actor.name} src={actor.image} />
-        </div>
-    </div>);
+  <div className="article" key={ actor.name }>
+    <div className="article__content">
+        <h2 className="article__heading">{ actor.name }</h2>
+        <p className="article__heading-sub">{ actor.dateOfBirth }</p>
+        <p className="article__heading-sub">{ actor.placeOfBirth }</p>
+        <p className="article__text">{ actor.biography }</p>
+    </div>
+    <div className="article__image">
+        <img alt={actor.name} src={actor.image} />
+    </div>
+  </div>);
 
 class Actors extends React.Component {
   constructor() {
-      super();
-      this.state = {
-        placeholder:'Search...'
-      };
+    super();
+    this.state = {
+      placeholder:'Search...'
+    };
   }
   render() {
     return (
       <section className="main">
         <input type="text" placeholder={this.state.placeholder} className="filter-search" />
         <div className="container">
-         {allActors}
+          {allActors}
         </div>
       </section>
     );
