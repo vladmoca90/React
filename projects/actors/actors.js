5c819659d23bcf999fa91c1c263281beb786a678
Vlad Mocanu <vladmoca90@gmail.com>
2023-02-04 00:26:50 +0000

    Deleted the extra spaces between the { }

--- projects/actors/actors.js
+++ projects/actors/actors.js
@@ -3,12 +3,12 @@ import './actors.css';
 import actors from './data';
 
 const allActors = actors.map((actor) =>
-  <div className="article" key={ actor.name }>
+  <div className="article" key={actor.name}>
     <div className="article__content">
-        <h2 className="article__heading">{ actor.name }</h2>
-        <p className="article__heading-sub">{ actor.dateOfBirth }</p>
-        <p className="article__heading-sub">{ actor.placeOfBirth }</p>
-        <p className="article__text">{ actor.biography }</p>
+        <h2 className="article__heading">{actor.name}</h2>
+        <p className="article__heading-sub">{actor.dateOfBirth}</p>
+        <p className="article__heading-sub">{actor.placeOfBirth}</p>
+        <p className="article__text">{actor.biography}</p>
     </div>
     <div className="article__image">
         <img alt={actor.name} src={actor.image} />
