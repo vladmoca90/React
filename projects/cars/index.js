ae16f6c14926996fcec03a0d4cd7a4c71f319e04
Vlad Mocanu <vladmoca90@gmail.com>
2023-07-06 02:01:47 +0100

    deleted 'cars'

--- projects/cars/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Showroom from './cars';
-// import * as serviceWorker from '../../serviceWorker';
-
-ReactDOM.render(<Showroom />, document.getElementById('root'));
-
-// serviceWorker.unregister();
