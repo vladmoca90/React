2ff4ec6518841a245828057d4ff812c46d7b49ba
Vlad Mocanu <vladmoca90@gmail.com>
2023-07-06 02:01:33 +0100

    Deleted destinations

--- projects/destinations/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Destinations from './destinations';
-// import * as serviceWorker from '../../serviceWorker';
-
-ReactDOM.render(<Destinations />, document.getElementById('root'));
-
-// serviceWorker.unregister();
