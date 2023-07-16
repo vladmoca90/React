9de18f09d606b6a9df22d7fe1445d6d43d840c77
Vlad Mocanu <vladmoca90@gmail.com>
2023-07-16 22:14:15 +0100

    Added gulpfile.js

---
+++ projects/gulpfile.js
@@ -0,0 +1,13 @@
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('content/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('content/'))
+});
+
+//Watch task
+gulp.task('default', function () {
+  return gulp.watch('content/**/*.scss', gulp.series('sass'));
+});
