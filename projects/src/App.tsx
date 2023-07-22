facceeb194c566cb1a20efa8dea0d594be4582fd
Vlad Mocanu <vladmoca90@gmail.com>
2023-07-22 23:17:05 +0100

    Added a new component to the App.tsx

--- projects/src/App.tsx
+++ projects/src/App.tsx
@@ -3,9 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <blockquote cite="Benjamin Franklin">
-        Tell me and I forget, teach me and I may remember, involve me and I learn.
-    </blockquote>
+    <div className="container">
+      <ProjectsPage />
+    </div>
   );
 }
 
