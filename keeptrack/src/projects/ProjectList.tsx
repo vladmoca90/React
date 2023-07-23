cfd13c704cd47361e97cf227f4669f0b46c18b74
Vlad Mocanu <vladmoca90@gmail.com>
2023-07-23 06:02:53 +0100

    Deleted useless code

--- keeptrack/src/projects/ProjectList.tsx
+++ keeptrack/src/projects/ProjectList.tsx
@@ -9,17 +9,7 @@ interface ProjectListProps {
 function ProjectList ({ projects }: ProjectListProps) {
     const items = projects.map(project => (
       <div key={project.id} className="colssm">
-      <div className="card">
-      <img src={project.imageUrl} alt={project.name} />
-       <section className="section dark">
-         <h5 className="strong">
-           <strong>{project.name}</strong>
-         </h5>
-         <p>{project.description}</p>
-        <p>Budget : {project.budget.toLocaleString()}</p>
-       </section>
-     </div>
-      <ProjectCard project={project}></ProjectCard>
+        <ProjectCard project={project}></ProjectCard>
       </div>
     ));
     return <div className="row">{items}</div>;
