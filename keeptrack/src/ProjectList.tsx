9c60005742e14723ca1018a2952dfb896352cbf7
Vlad Mocanu <vladmoca90@gmail.com>
2023-07-23 15:42:50 +0100

    Formatted the documents

--- keeptrack/src/ProjectList.tsx
+++ keeptrack/src/ProjectList.tsx
@@ -6,13 +6,13 @@ interface ProjectListProps {
   projects: Projects[];
 }
 
-function ProjectList ({ projects }: ProjectListProps) {
-    const items = projects.map(project => (
-      <div key={project.id} className="colssm">
-        <ProjectCard project={project}></ProjectCard>
-      </div>
-    ));
-    return <div className="row">{items}</div>;
+function ProjectList({ projects }: ProjectListProps) {
+  const items = projects.map(project => (
+    <div key={project.id} className="colssm">
+      <ProjectCard project={project}></ProjectCard>
+    </div>
+  ));
+  return <div className="row">{items}</div>;
 
 }
 
