3cafe28237cb8eb2a6b231618f768b8cc13aa649
Vlad Mocanu <vladmoca90@gmail.com>
2023-07-23 06:08:19 +0100

    Added an edit button

--- keeptrack/src/projects/ProjectCard.tsx
+++ keeptrack/src/projects/ProjectCard.tsx
@@ -2,27 +2,31 @@ import React from 'react';
 import { Projects } from './Projects';
 
 function formatDescription(description: string): string {
-  return description.substring(0, 60) + '...';
+    return description.substring(0, 60) + '...';
 }
 
 interface ProjectCardProps {
-  project: Projects;
+    project: Projects;
 }
 
 function ProjectCard(props: ProjectCardProps) {
-  const { project } = props;
-  return (
-    <div className="card">
-      <img src={project.imageUrl} alt={project.name} />
-      <section className="section dark">
-        <h5 className="strong">
-          <strong>{project.name}</strong>
-        </h5>
-        <p>{formatDescription(project.description)}</p>
-        <p>Budget : {project.budget.toLocaleString()}</p>
-      </section>
-    </div>
-  );
+    const { project } = props;
+    return (
+        <div className="card">
+            <img src={project.imageUrl} alt={project.name} />
+            <section className="section dark">
+                <h5 className="strong">
+                    <strong>{project.name}</strong>
+                </h5>
+                <p>{formatDescription(project.description)}</p>
+                <p>Budget : {project.budget.toLocaleString()}</p>
+                <button className=" bordered">
+                    <span className="icon-edit "></span>
+                    Edit
+                </button>
+            </section>
+        </div>
+    );
 }
 
 export default ProjectCard;
