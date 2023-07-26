a3ecec3f23295aa3529d0fc49dce602556071437
Vlad Mocanu <vladmoca90@gmail.com>
2023-07-26 22:44:39 +0100

    working on the const airport =

--- my-app/src/app/airlines-destinations.tsx
+++ my-app/src/app/airlines-destinations.tsx
@@ -3,35 +3,33 @@ import './airlines-destinations.css';
 import allFlights from './data';
 
 const airport = allFlights.map(allFlight => {
-  return (
     <tr>
-      <th className="flight-row">{allFlights.indexOf(allFlight) + 1}</th>
+      <th scope="row">{allFlights.indexOf(allFlight) + 1}</th>
       <td>{allFlight.airline}</td>
       <td>{allFlight.destinations}</td>
     </tr>
-  );
-}
+});
 
 const Destinations = () => {
-    return (
-      <section className="main">
-        <div className="container">
-          <table className="table table-hover">
-            <thead className="table-header">
-              <th>#</th>
-              <th>Name</th>
-              <th>Destinations</th>
-            </thead>
-            <tbody className="table-content">
-              {airport}
-            </tbody>
-          </table>
-          <div className="total-destinations">
-            <span>Number of airlines: {allFlights.length}</span>
-          </div>
+  return (
+    <section className="main">
+      <div className="container">
+        <table className="table table-hover">
+          <thead className="table-header">
+            <th>#</th>
+            <th>Name</th>
+            <th>Destinations</th>
+          </thead>
+          <tbody className="table-content">
+            {airport}
+          </tbody>
+        </table>
+        <div className="total-destinations">
+          <span>Number of airlines: {allFlights.length}</span>
         </div>
-      </section>
-    );
+      </div>
+    </section>
+  );
 }
 
 export default Destinations;
