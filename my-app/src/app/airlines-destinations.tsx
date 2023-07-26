80ad70e6e03edb8f24ca95886270ceeffe669bc4
Vlad Mocanu <vladmoca90@gmail.com>
2023-07-26 21:57:23 +0100

    Will try to rewrite from scratch

--- my-app/src/app/airlines-destinations.tsx
+++ my-app/src/app/airlines-destinations.tsx
@@ -2,36 +2,12 @@ import React from 'react';
 import './airlines-destinations.css';
 import allFlights from './data';
 
-const airport = allFlights.map((allFlight) => 
-                    <tr>
-                      <th scope="row">{allFlight.indexOf(allFlight) + 1}</th>
-                      <td>{allFlight.airline}</td>
-                      <td>{allFlight.destinations}</td>
-                    </tr>
-                  );
-
-class Destinations extends React.Component {
-  render() {
-    return (
-       <section className="main">
-        <div className="container">
-          <table className="table table-hover">
-            <thead className="table-header">
-              <th>#</th>
-              <th>Name</th>
-              <th>Destinations</th>
-            </thead>
-            <tbody className="table-content">
-                {airport}
-            </tbody>
-          </table>
-          <div className="total-destinations">
-            <span>Number of airlines: {allFlights.length}</span>
-          </div>
-        </div>
-      </section>
-    );
-  }
-}
+const airport = allFlights.map((allFlight) =>
+  <tr>
+    <th scope="row">{allFlight.indexOf(allFlight) + 1}</th>
+    <td>{allFlight.airline}</td>
+    <td>{allFlight.destinations}</td>
+  </tr>
+);
 
 export default Destinations;
