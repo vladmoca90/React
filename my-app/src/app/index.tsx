59c0d928e84eb2d524712b13d2c1e4df8ad46371
Vlad Mocanu <vladmoca90@gmail.com>
2023-07-26 23:17:08 +0100

    Fixed the ReactDOM issue

--- my-app/src/app/index.tsx
+++ my-app/src/app/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom/client";
 import Destinations from './airlines-destinations';
-// import * as serviceWorker from '../../serviceWorker';
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(<Destinations />, root);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <Destinations />
+  </React.StrictMode>
+);
 
-serviceWorker.unregister();
+reportWebVitals();
