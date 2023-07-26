18072658ee7fc86b09149ddcfa13489cfa1add7a
Vlad Mocanu <vladmoca90@gmail.com>
2023-07-26 21:43:15 +0100

    Created the airlines-destinations and index .tsx files

---
+++ my-app/src/app/airlines-destinations.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './airlines-destinations.css';
+import flights from './data';
+
+const allFlights = flights.map((flight) => 
+                    <tr>
+                      <th scope="row">{flights.indexOf(flight) + 1}</th>
+                      <td>{flight.airline}</td>
+                      <td>{flight.destinations}</td>
+                    </tr>
+                  );
+
+class Destinations extends React.Component {
+  render() {
+    return (
+       <section className="main">
+        <div className="container">
+          <table className="table table-hover">
+            <thead className="table-header">
+              <th>#</th>
+              <th>Name</th>
+              <th>Destinations</th>
+            </thead>
+            <tbody className="table-content">
+                {allFlights}
+            </tbody>
+          </table>
+          <div class="total-destinations">
+            <span>Number of airlines: {flights.length}</span>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Destinations;
