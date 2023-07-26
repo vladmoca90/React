a55da5fb6ef83fd7a100390ff03367a7b2e8791c
Vlad Mocanu <vladmoca90@gmail.com>
2023-07-26 22:37:22 +0100

    Fixed the table bugs

--- my-app/src/app/airlines-destinations.tsx
+++ my-app/src/app/airlines-destinations.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import './airlines-destinations.css';
 import allFlights from './data';
 
-const airport = allFlights.map((allFlights: []) =>
-  <tr>
-    <th scope="row">{allFlight.indexOf(allFlights) + 1}</th>
-    <td>{allFlights.airline}</td>
-    <td>{allFlights.destinations}</td>
-  </tr>
-);
+const airport = allFlights.map(allFlight => {
+  return (
+    <tr>
+      <th className="flight-row">{allFlights.indexOf(allFlight) + 1}</th>
+      <td>{allFlight.airline}</td>
+      <td>{allFlight.destinations}</td>
+    </tr>
+  );
+}
 
 const Destinations = () => {
     return (
