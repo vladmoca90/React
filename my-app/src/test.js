91af539a0d225f0eb0822a08f76ee68f8fca3231
Vlad Mocanu <vladmoca90@gmail.com>
2023-07-30 01:22:46 +0100

    It works now! The total number of destinations is 45

--- my-app/src/test.js
+++ my-app/src/test.js
@@ -105,6 +105,19 @@ const totalNumberOfDestinations = () => {
     if(flights.length == 0) {
         throw new Error('The list of flights must be provided!');
     }
+
+    let counter = 0;
+
+    for(let i in flights) {
+        if(typeof flights[i].destinations === 'string') {
+            counter += 1;
+        }
+        if(typeof flights[i].destinations === 'object') {
+            counter += flights[i].destinations.length;
+        }
+    }
+
+    return counter;
 }
 
 totalNumberOfDestinations();
