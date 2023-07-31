e1cf2f3cb9d90a6db54a2ed8d1310e24147c7b86
Vlad Mocanu <vladmoca90@gmail.com>
2023-07-31 22:21:37 +0100

    Rewrote as arrow function, shorter

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -5,15 +5,13 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './airlines-destinations.css';
 
 //Populates the entire table with all the data from flights = []
-const allFlights = flights.map((flight) => {
-  return (
+const allFlights = flights.map((flight) => 
     <tr key={flight.id}>
       <td className="airline-number">{flights.indexOf(flight) + 1}</td>
       <td className="airline-name">{flight.airline}</td>
       <td className="airline-destinations-list">{flight.destinations.toString().replace(/,/g, ', ')}</td>
     </tr>
-  );
-});
+);
 
 //Calculates the total number of destinations
 const totalNumberOfDestinations = () => {
