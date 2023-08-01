428efe5e8bccf519c7d63ec5c9f67eab42348579
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-01 23:14:35 +0100

    Deleted a useless row

--- my-app/src/app/wine-sell/imageProfile.tsx
+++ my-app/src/app/wine-sell/imageProfile.tsx
@@ -1,6 +1,7 @@
 import wines from './data';
 import Image from 'next/image';
 
+
 export default function ProfileImg() {
     return (
         <Image src={wines.img} alt="img" className="product-img"/>
