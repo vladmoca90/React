86d825b078a580ce18dd34e3faaa4f9565740d2e
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-01 22:33:25 +0100

    Added the () for the return statement

--- my-app/src/app/wine-sell/imageProfile.tsx
+++ my-app/src/app/wine-sell/imageProfile.tsx
@@ -2,5 +2,7 @@ import wines from './data';
 import Image from 'next/image';
 
 export default function profileImg() {
-    return <Image src={wines.imgName} alt="img" className="product-img"/>;
+    return (
+        <Image src={wines.imgName} alt="img" className="product-img"/>
+    );
 }
