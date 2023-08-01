08f392a8fec7a35902f4c49e6c89a7847e260ee3
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-01 22:45:54 +0100

    Formatted the data.tsx file

--- my-app/src/app/wine-sell/data.tsx
+++ my-app/src/app/wine-sell/data.tsx
@@ -13,27 +13,27 @@ const wines: winesProp[] = [
     {
         id: 2,
         name: 'Chilean Dry White',
-         img: './wine-sell-img/italian-red.jpg',
+        img: './wine-sell-img/italian-red.jpg',
     },
     {
         id: 3,
         name: 'NZ Savignon Blanc',
-         img: './wine-sell-img/italian-red.jpg',
+        img: './wine-sell-img/italian-red.jpg',
     },
     {
         id: 4,
         name: 'Spanish Dry White',
-         img: './wine-sell-img/italian-red.jpg',
+        img: './wine-sell-img/italian-red.jpg',
     },
     {
         id: 5,
         name: 'Californian Chardonnay',
-         img: './wine-sell-img/italian-red.jpg',
+        img: './wine-sell-img/italian-red.jpg',
     },
     {
         id: 6,
         name: 'Pinot Noir Romania',
-         img: './wine-sell-img/italian-red.jpg',
+        img: './wine-sell-img/italian-red.jpg',
     }
 ];
 
