a96bbf304618eee26936f750a51afc9a93053ab7
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-01 22:29:36 +0100

    Working

--- my-app/src/app/wine-sell/imageProfile.tsx
+++ my-app/src/app/wine-sell/imageProfile.tsx
@@ -1,8 +1,12 @@
 import wines from './data';
 import Image from 'next/image';
-   
+
 export default function profileImg() {
     return (
-        <Image src={wines.imgName} alt="img" className="product-img" />
+        <Image
+            src={wines.imgName}
+            alt="img"
+            className="product-img"
+        />
     );
-  }
+}
