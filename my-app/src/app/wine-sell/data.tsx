402fa244a8eed6766eb0a3fff01157596c83999d
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-01 20:57:15 +0100

    Created the data and the interface

--- my-app/src/app/wine-sell/data.tsx
+++ my-app/src/app/wine-sell/data.tsx
@@ -1,9 +1,33 @@
 interface wines {
-
+    name: string;
+    image: string;
 }
 
-const allWines = [
-
+const allWines: wines[] = [
+    {
+        name: 'Italian Red Wine',
+        image: '',
+    },
+    {
+        name: 'Chilean Dry White',
+        image: '',
+    },
+    {
+        name: 'NZ Savignon Blanc',
+        image: '',
+    },
+    {
+        name: 'Spanish Dry White',
+        image: '',
+    },
+    {
+        name: 'Californian Chardonnay',
+        image: '',
+    },
+    {
+        name: 'Pinot Noir Romania',
+        image: '',
+    }
 ];
 
 export default allWines;
