02458b04ae93a65e2901c3a1ed948cb815df5dce
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-01 21:58:22 +0100

    Trying to create an image component

--- my-app/src/app/wine-sell/wine-sell.tsx
+++ my-app/src/app/wine-sell/wine-sell.tsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect, useCallback} from 'react';
 import wines from './data';
+import Image from './imageProfile';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './wine-sell.css';
@@ -11,7 +12,7 @@ const giveWines = wines.map((wine) => {
                 <h3 className="product-title">{wine.name}</h3>
             </div>
             <div className="product-description__right">
-                <img alt="img" className="product-img" src={wine.image} />
+                <Image />
             </div>
         </div>
     );
