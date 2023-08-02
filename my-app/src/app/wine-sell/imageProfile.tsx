2af6551e400f7f65ff23a8c06928ccec144d3ce9
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-02 21:09:19 +0100

    Wrote the return statement without ( )

--- my-app/src/app/wine-sell/imageProfile.tsx
+++ my-app/src/app/wine-sell/imageProfile.tsx
@@ -2,11 +2,9 @@ import wines from './data';
 import Image from 'next/image';
 
 const wineImg = wines.map((wine) => { 
-    return <Image key={wine.id} src={wine.img} alt="img" className="product-img" />
+    return <Image key={wine.id} src={wine.img} alt="img" className="product-img" />;
 }); 
 
 export default function ProfileImg() {
-    return (
-       wineImg
-    );
+    return wineImg;
 }
