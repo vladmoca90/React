e85262c0f5a160b7094e0f50d18b1325360f087b
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-02 21:01:02 +0100

    Added a space before the /> ending

--- my-app/src/app/wine-sell/imageProfile.tsx
+++ my-app/src/app/wine-sell/imageProfile.tsx
@@ -2,7 +2,7 @@ import wines from './data';
 import Image from 'next/image';
 
 const wineImg = wines.map((wine) => { 
-    return <Image key={wine.id} src={wine.img} alt="img" className="product-img"/>
+    return <Image key={wine.id} src={wine.img} alt="img" className="product-img" />
 }); 
 
 export default function ProfileImg() {
