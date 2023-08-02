d5228c9294e8ea05758d511aa529374d74b25b1a
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-02 23:39:58 +0100

    Added - again for image names

--- my-app/src/app/data.tsx
+++ my-app/src/app/data.tsx
@@ -8,32 +8,32 @@ const wines: winesProp[] = [
     {
         id: 1,
         name: 'Italian Red Wine',
-        img: './wineImages/italianred.jpg',
+        img: './wineImages/italian-red.jpg',
     },
     {
         id: 2,
         name: 'Chilean Dry White',
-        img: './wineImages/chileandrywhite.jpg',
+        img: './wineImages/chilean-dry-white.jpg',
     },
     {
         id: 3,
         name: 'NZ Savignon Blanc',
-        img: './wineImages/nzsavignonblanc.jpg',
+        img: './wineImages/nz-savignon-blanc.jpg',
     },
     {
         id: 4,
         name: 'Spanish Dry White',
-        img: './wineImages/spanishdrywhite.jpg',
+        img: './wineImages/spanish-dry-white.jpg',
     },
     {
         id: 5,
         name: 'Californian Chardonnay',
-        img: './wineImages/californianchardonnay.jpg',
+        img: './wineImages/californian-chardonnay.jpg',
     },
     {
         id: 6,
         name: 'Pinot Noir Romania',
-        img: './wineImages/romaniapinotnoir.jpg',
+        img: './wineImages/romania-pinot-noir.jpg',
     }
 ];
 
