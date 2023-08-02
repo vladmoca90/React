82495f00a9ae2bbf2d358c28f223f3baea92e95b
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-02 21:00:25 +0100

    Created a constant to include the <image />

--- my-app/src/app/wine-sell/imageProfile.tsx
+++ my-app/src/app/wine-sell/imageProfile.tsx
@@ -1,9 +1,12 @@
 import wines from './data';
 import Image from 'next/image';
 
+const wineImg = wines.map((wine) => { 
+    return <Image key={wine.id} src={wine.img} alt="img" className="product-img"/>
+}); 
 
 export default function ProfileImg() {
     return (
-        <Image src={wines.img} alt="img" className="product-img"/>
+       wineImg
     );
 }
