a49aff150d34305ce26d2e54086c9ac6b8521425
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-04 07:47:25 +0100

    Added the new code here as well

--- my-app/src/app/wine-sell/wine-sell.tsx
+++ my-app/src/app/wine-sell/wine-sell.tsx
@@ -6,28 +6,28 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './wine-sell.css';
 
 //A constant that populates the product with wines
-const getWines = wines.map((wine) => {
+const getWines = wines.map((wine, index) => {
     return (
-        <div className="product" key={wine.id}>
+        <div className="product" key={index}>
             <div className="product-description__left">
                 <h3 className="product-title">{wine.name}</h3>
             </div>
             <div className="product-description__right">
-                <img alt="wine" className="product-img" key={wine.id} src={wine.img} />
+                <img alt="wine" className="product-img" key={index} src={wine.img} />
             </div>
         </div>
     );
 });
 
 //A constant that populates the dropdown
-const winesDropdown = wines.map((wine) => {
-    return <option key={wine.id}>{wine.name}</option>
+const winesDropdown = wines.map((wine, index) => {
+    return <option key={index}>{wine.name}</option>
 });
 
 export default function wineSell() {
     return (
         <section className="box">
-            <select aria-label="allProducts" id="productsList">
+            <select aria-label="winesDropdown" id="productsList">
                 <option>All products</option>
                 {winesDropdown}
             </select>
