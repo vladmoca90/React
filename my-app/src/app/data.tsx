f704030dbe136e0a8cf97f7eef57df2d1e74276a
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-04 07:55:54 +0100

    Added all the images in the wine-sell folder

--- my-app/src/app/data.tsx
+++ my-app/src/app/data.tsx
@@ -6,27 +6,27 @@ interface winesProp {
 const wines: winesProp[] = [
     {
         name: 'Italian Red Wine',
-        img: './wineImages/italian-red.jpg',
+        img: './wine-sell/wineImages/italian-red.jpg',
     },
     {
         name: 'Chilean Dry White',
-        img: './wineImages/chilean-dry-white.jpg',
+        img: './wine-sell/wineImages/chilean-dry-white.jpg',
     },
     {
         name: 'NZ Savignon Blanc',
-        img: './wineImages/nz-savignon-blanc.jpg',
+        img: './wine-sell/wineImages/nz-savignon-blanc.jpg',
     },
     {
         name: 'Spanish Dry White',
-        img: './wineImages/spanish-dry-white.jpg',
+        img: './wine-sell/wineImages/spanish-dry-white.jpg',
     },
     {
         name: 'Californian Chardonnay',
-        img: './wineImages/californian-chardonnay.jpg',
+        img: './wine-sell/wineImages/californian-chardonnay.jpg',
     },
     {
         name: 'Pinot Noir Romania',
-        img: './wineImages/romania-pinot-noir.jpg',
+        img: './wine-sell/wineImages/romania-pinot-noir.jpg',
     }
 ];
 
