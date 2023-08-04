3a78f2672465b7cfebda5f3a0f4927c6e9422226
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-04 08:05:17 +0100

    Images still don't display

--- my-app/src/app/data.tsx
+++ my-app/src/app/data.tsx
@@ -6,27 +6,27 @@ interface winesProp {
 const wines: winesProp[] = [
     {
         name: 'Italian Red Wine',
-        img: './wine-sell/wineImages/italian-red.jpg',
+        img: 'wine-sell/wineImages/italian-red.jpg',
     },
     {
         name: 'Chilean Dry White',
-        img: './wine-sell/wineImages/chilean-dry-white.jpg',
+        img: 'wine-sell/wineImages/chilean-dry-white.jpg',
     },
     {
         name: 'NZ Savignon Blanc',
-        img: './wine-sell/wineImages/nz-savignon-blanc.jpg',
+        img: 'wine-sell/wineImages/nz-savignon-blanc.jpg',
     },
     {
         name: 'Spanish Dry White',
-        img: './wine-sell/wineImages/spanish-dry-white.jpg',
+        img: 'wine-sell/wineImages/spanish-dry-white.jpg',
     },
     {
         name: 'Californian Chardonnay',
-        img: './wine-sell/wineImages/californian-chardonnay.jpg',
+        img: 'wine-sell/wineImages/californian-chardonnay.jpg',
     },
     {
         name: 'Pinot Noir Romania',
-        img: './wine-sell/wineImages/romania-pinot-noir.jpg',
+        img: 'wine-sell/wineImages/romania-pinot-noir.jpg',
     }
 ];
 
