203ce044a5896cc7ea2f58042d38016b665d4489
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-04 07:42:01 +0100

    Replaced the id with the index value

--- my-app/src/app/data.tsx
+++ my-app/src/app/data.tsx
@@ -1,37 +1,30 @@
 interface winesProp {
-    id: number;
     name: string;
     img: string;
 }
 
 const wines: winesProp[] = [
     {
-        id: 1,
         name: 'Italian Red Wine',
         img: './wineImages/italian-red.jpg',
     },
     {
-        id: 2,
         name: 'Chilean Dry White',
         img: './wineImages/chilean-dry-white.jpg',
     },
     {
-        id: 3,
         name: 'NZ Savignon Blanc',
         img: './wineImages/nz-savignon-blanc.jpg',
     },
     {
-        id: 4,
         name: 'Spanish Dry White',
         img: './wineImages/spanish-dry-white.jpg',
     },
     {
-        id: 5,
         name: 'Californian Chardonnay',
         img: './wineImages/californian-chardonnay.jpg',
     },
     {
-        id: 6,
         name: 'Pinot Noir Romania',
         img: './wineImages/romania-pinot-noir.jpg',
     }
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -6,22 +6,22 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './wine-sell.css';
 
 //A constant that populates the product with wines
-const getWines = wines.map((wine) => {
+const getWines = wines.map((wine, index) => {
     return (
-        <div className="product" key={wine.id}>
+        <div className="product" key={index}>
             <div className="product-description__left">
                 <h3 className="product-title">{wine.name}</h3>
             </div>
             <div className="product-description__right">
-                <img alt="wine" className="product-img" key={wine.id} src={wine.img} />
+                <img alt="wine" className="product-img" key={index} src={wine.img} />
             </div>
         </div>
     );
 });
 
 //A constant that populates the dropdown
-const winesDropdown = wines.map((wine) => {
-    return <option key={wine.id}>{wine.name}</option>
+const winesDropdown = wines.map((wine, index) => {
+    return <option key={index}>{wine.name}</option>
 });
 
 export default function wineSell() {
