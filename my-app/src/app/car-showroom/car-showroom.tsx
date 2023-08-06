4c19e902d33130a7d99634fc8834dbe1eed1f50a
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-06 03:24:39 +0100

    Created a sample for a future car showroom project

---
+++ my-app/src/app/car-showroom/car-showroom.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable @next/next/no-img-element */
+
+/*
+import React from 'react';
+import cars from '../data';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './car-showroom.css';
+
+export default function getVehicles() {
+    return (
+        
+    );
+}
+*/
