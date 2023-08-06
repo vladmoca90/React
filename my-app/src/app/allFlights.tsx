519780ba03c82f043950f2ecc5637de405c8b3c3
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-06 23:47:13 +0100

    Wrote flight.index as index only

--- my-app/src/app/allFlights.tsx
+++ my-app/src/app/allFlights.tsx
@@ -4,7 +4,7 @@ import { allFlights } from './data';
 //Populates the entire table with all the data from flights = []
 export const totalFlights = allFlights.map((flight, index) => {
   return (
-    <tr key={flight.index}>
+    <tr key={index}>
       <td className="airline-number">{flight.indexOf(flight) + 1}</td>
       <td className="airline-name">{flight.airline}</td>
       <td className="airline-destinations-list">{flight.destinations.toString().replace(/,/g, ', ')}</td>
