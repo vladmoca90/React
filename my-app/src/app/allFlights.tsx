82a6419bcac7e0952a46e3672ae2158bbfe54a44
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-06 23:27:03 +0100

    Removed the id and added the index instead

--- my-app/src/app/allFlights.tsx
+++ my-app/src/app/allFlights.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import flights from './data';
 
 //Populates the entire table with all the data from flights = []
-export const allFlights = flights.map((flight) => {
+export const allFlights = flights.map((flight, index) => {
   return (
-    <tr key={flight.id}>
+    <tr key={flight.index}>
       <td className="airline-number">{flights.indexOf(flight) + 1}</td>
       <td className="airline-name">{flight.airline}</td>
       <td className="airline-destinations-list">{flight.destinations.toString().replace(/,/g, ', ')}</td>
