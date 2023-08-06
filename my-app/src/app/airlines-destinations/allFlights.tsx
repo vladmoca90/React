df7bc18d4acc6fba50a04be66443adc3f14855c6
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-06 23:03:45 +0100

    Created two separate .tsx files for future components

---
+++ my-app/src/app/airlines-destinations/allFlights.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import flights from './data';
+
+//Populates the entire table with all the data from flights = []
+export const allFlights = flights.map((flight) => {
+  return (
+    <tr key={flight.id}>
+      <td className="airline-number">{flights.indexOf(flight) + 1}</td>
+      <td className="airline-name">{flight.airline}</td>
+      <td className="airline-destinations-list">{flight.destinations.toString().replace(/,/g, ', ')}</td>
+    </tr>
+  );
+});
