2748f646a6cbbd9c0ee2223d9688460877463dd8
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-06 20:55:27 +0100

    Renamed airlines-destinations.tsx to page.tsx

--- my-app/src/app/airlines-destinations/index.tsx
+++ my-app/src/app/airlines-destinations/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
-import Destinations from './airlines-destinations';
+import Destinations from './page';
 
 //import reportWebVitals from "./reportWebVitals";
 
