4d2063f29292291ecbe64286d05bf86046767ae0
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-06 17:27:21 +0100

    Changed the path to the page.tsx

--- my-app/src/app/index.tsx
+++ my-app/src/app/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
-import GetWines from './wine-sell/wine-sell';
+import GetWines from './page';
 
 //import reportWebVitals from "./reportWebVitals";
 
