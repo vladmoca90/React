5761b60609fbed11802b5a82e92d844c858a34c8
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-06 23:04:10 +0100

    Saved the new file with the new import

--- my-app/src/app/airlines-destinations/page.tsx
+++ my-app/src/app/airlines-destinations/page.tsx
@@ -1,39 +1,9 @@
-import React, {useState, useEffect, useCallback} from 'react';
-import flights from './data';
+import React from 'react';
+import { allFlights } from './allFlights';
+import { totalNumberOfDestinations } from './totalNumberOfDestinations';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './airlines-destinations.css';
-
-//Populates the entire table with all the data from flights = []
-const allFlights = flights.map((flight) => {
-  return (
-    <tr key={flight.id}>
-      <td className="airline-number">{flights.indexOf(flight) + 1}</td>
-      <td className="airline-name">{flight.airline}</td>
-      <td className="airline-destinations-list">{flight.destinations.toString().replace(/,/g, ', ')}</td>
-    </tr>
-  );
-});
-
-//Calculates the total number of destinations
-const totalNumberOfDestinations = () => {
-  if (flights.length == 0) {
-    throw new Error('The list of flights must be provided!');
-  }
-
-  let counter = 0;
-
-  for (let i in flights) {
-    if (typeof flights[i].destinations === 'string') {
-      counter += 1;
-    }
-    if (typeof flights[i].destinations === 'object') {
-      counter += flights[i].destinations.length;
-    }
-  }
-
-  return counter;
-}
 
 //Main function, that shows the table structure
 export default function Destinations() {
