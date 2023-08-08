ca555215cbafd05b2e933c4b09e6890b81e7698b
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-08 01:19:37 +0100

    Added /* eslint-disable @next/next/no-img-element */ "use client"; in DestinationsComponent.tsx

--- my-app/src/app/DestinationsComponent.tsx
+++ my-app/src/app/DestinationsComponent.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
 import React from 'react';
 import { TotalDestinations } from './TotalDestinations';
 import { allFlights } from './data';
