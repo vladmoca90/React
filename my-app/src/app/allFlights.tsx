611ff4a70832a2ce90d3d082bba7fcfdd2c1aeee
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-08 01:12:14 +0100

    Deleted the .indexOf()

--- my-app/src/app/allFlights.tsx
+++ my-app/src/app/allFlights.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import { allFlights } from './data';
 
 //Populates the entire table with all the data from flights = []
-export const totalFlights = allFlights.map((flight, index) => {
-  return (
-    <tr key={index}>
-      <td className="airline-number">{flight.indexOf(flight) + 1}</td>
-      <td className="airline-name">{flight.airline}</td>
-      <td className="airline-destinations-list">{flight.destinations.toString().replace(/,/g, ', ')}</td>
-    </tr>
-  );
-});
+export const totalFlights = allFlights.map((flight, index) => (
+  <tr key={index}>
+    <td className="airline-number">{flight.index + 1}</td>
+    <td className="airline-name">{flight.airline}</td>
+    <td className="airline-destinations-list">{flight.destinations.toString().replace(/,/g, ', ')}</td>
+  </tr>
+));
