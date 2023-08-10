a31ae82013b19bf5b1a4a1ea018407f6966ab40b
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-10 21:13:31 +0100

    Added 3 cars as data

--- my-app/src/app/car-showroom/data.tsx
+++ my-app/src/app/car-showroom/data.tsx
@@ -7,9 +7,21 @@ export interface Car {
 
 export const AllCars: Car[] = [
     {
-        make: '',
-        model: '',
-        price: 14999,
-        year: 2012,
+        make: 'Audi',
+        model: 'TT 2.0T FSI 2dr',
+        price: 2989,
+        year: 2007,
+    },
+    {
+        make: 'Mercedes-Benz',
+        model: 'S Class S500 4dr Auto',
+        price: 6489,
+        year: 2006,
+    },
+    {
+        make: 'Vauxhall',
+        model: 'Mokka 1.6 CDTi Limited Edition 5dr',
+        price: 5789,
+        year: 2015,
     }
 ];
