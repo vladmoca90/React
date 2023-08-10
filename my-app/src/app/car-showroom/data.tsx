374e727f362176b2e7ba0981ece58b95044f3360
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-10 21:03:19 +0100

    Started to create the data for the Car Showroom

---
+++ my-app/src/app/car-showroom/data.tsx
@@ -0,0 +1,15 @@
+export interface Car {
+    make: string;
+    model: string;
+    price: number;
+    year: number;
+}
+
+export const AllCars: Car[] = [
+    {
+        make: '',
+        model: '',
+        price: 14999,
+        year: 2012,
+    }
+];
