71f29d3eccdf168dfc0f7d28e5d07faeed963b46
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-10 20:58:47 +0100

    Put the import CarsComponent from './CarsComponent'; on the row under the React import

--- my-app/src/app/car-showroom/page.tsx
+++ my-app/src/app/car-showroom/page.tsx
@@ -1,10 +1,10 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 import React from 'react';
+import CarsComponent from './CarsComponent';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './cars-showroom.css';
-import CarsComponent from './CarsComponent';
 
 export default function CarsPage() {
     return (<CarsComponent />);
