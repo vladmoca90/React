cc7b7aeed8441d2c14a3a7dafa5174fe827d22a9
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-10 21:15:24 +0100

    Added the img property for images

--- my-app/src/app/car-showroom/data.tsx
+++ my-app/src/app/car-showroom/data.tsx
@@ -1,4 +1,5 @@
 export interface Car {
+    img: string,
     make: string;
     model: string;
     price: number;
@@ -7,18 +8,21 @@ export interface Car {
 
 export const AllCars: Car[] = [
     {
+        img: '',
         make: 'Audi',
         model: 'TT 2.0T FSI 2dr',
         price: 2989,
         year: 2007,
     },
     {
+        img: '',
         make: 'Mercedes-Benz',
         model: 'S Class S500 4dr Auto',
         price: 6489,
         year: 2006,
     },
     {
+        img: '',
         make: 'Vauxhall',
         model: 'Mokka 1.6 CDTi Limited Edition 5dr',
         price: 5789,
