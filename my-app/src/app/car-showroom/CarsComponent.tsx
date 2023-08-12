94dc02f9dfc6e645ce3ef00a316bbe3f0bc25a11
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-12 20:07:51 +0100

    Added the React, { useCallback, useEffect, useState }  to CarsComponent.tsx

--- my-app/src/app/car-showroom/CarsComponent.tsx
+++ my-app/src/app/car-showroom/CarsComponent.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { AllCars } from './data';
 
 export default function CarsComponent() {
