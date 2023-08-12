b763d3bccc2d3326b990b4bc15c11ab1212c0fbc
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-12 20:05:08 +0100

    Added 3 more cars to the object data

--- my-app/src/app/car-showroom/data.tsx
+++ my-app/src/app/car-showroom/data.tsx
@@ -15,11 +15,32 @@ export const AllCars: Car[] = [
         year: 2007,
     },
     {
+        img: 'land-rover.jpg',
+        make: 'Land Rover',
+        model: 'Discovery 2.7 Td V6 HSE 5dr Auto',
+        price: 4489,
+        year: 2009,
+    },
+    {
         img: 'mercedes-benz.jpg',
         make: 'Mercedes-Benz',
         model: 'S Class S500 4dr Auto',
         price: 6489,
         year: 2006,
+    },
+    {
+        img: 'nissan.jpg',
+        make: 'Nissan',
+        model: 'Fairlady Turbo 3.5 V6 300 GT 3d',
+        price: 9989,
+        year: 2007,
+    },
+    {
+        img: 'skoda.jpg',
+        make: 'Skoda',
+        model: 'Fabia 1.2 TSI SE 5dr',
+        price: 2489,
+        year: 2011,
     },
     {
         img: 'vauxhall.jpg',
