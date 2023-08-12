83bf3fe629c8acdff375322ebad46f4d09ae212f
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-12 11:04:56 +0100

    Added the .jpg images to the data.tsx

--- my-app/src/app/car-showroom/data.tsx
+++ my-app/src/app/car-showroom/data.tsx
@@ -8,21 +8,21 @@ export interface Car {
 
 export const AllCars: Car[] = [
     {
-        img: '',
+        img: 'audi.jpg',
         make: 'Audi',
         model: 'TT 2.0T FSI 2dr',
         price: 2989,
         year: 2007,
     },
     {
-        img: '',
+        img: 'mercedes-benz.jpg',
         make: 'Mercedes-Benz',
         model: 'S Class S500 4dr Auto',
         price: 6489,
         year: 2006,
     },
     {
-        img: '',
+        img: 'vauxhall.jpg',
         make: 'Vauxhall',
         model: 'Mokka 1.6 CDTi Limited Edition 5dr',
         price: 5789,
