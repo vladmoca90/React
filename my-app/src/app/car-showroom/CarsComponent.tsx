eb0f5c626350ec26562d784c239570f3adecdfcc
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-12 11:19:43 +0100

    Added the .map()

--- my-app/src/app/car-showroom/CarsComponent.tsx
+++ my-app/src/app/car-showroom/CarsComponent.tsx
@@ -3,16 +3,19 @@
 import React, { useCallback, useState } from 'react';
 import { AllCars } from './data';
 
-export default function CarsComponent () {
-    return (
-        <div className="showroom-container">
-            <div className='car-container'>
-                <h3 className="car-title"></h3>
-                <p className="car-price"></p>
-                <div>
-                    <img alt="car" className="product-img" key={index} src={"./carShowroom/" + car.img} />
+export default function CarsComponent() {
+    {AllCars.map((car, index) => {
+        return (
+            <div className="showroom-container" key={index}>
+                <div className='car-container'>
+                    <h3 className="car-title"></h3>
+                    <p className="car-price"></p>
+                    <div>
+                        <img alt="car" className="product-img" key={index} src={"./carShowroom/" + car.img} />
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+            );
+        });
+    }
 }
