3667946f460f37040c90dddce89dd259f780f8fe
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-12 11:06:40 +0100

    removed the { } from CarsComponent

--- my-app/src/app/car-showroom/page.tsx
+++ my-app/src/app/car-showroom/page.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 import React from 'react';
-import { CarsComponent } from './CarsComponent';
+import CarsComponent from './CarsComponent';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './cars-showroom.css';
