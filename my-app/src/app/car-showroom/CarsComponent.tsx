1a906f4576994c4c1d1e5232ff2cf982bd7edcd8
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-12 11:17:17 +0100

    Created a brief layout of the html structure

--- my-app/src/app/car-showroom/CarsComponent.tsx
+++ my-app/src/app/car-showroom/CarsComponent.tsx
@@ -3,8 +3,16 @@
 import React, { useCallback, useState } from 'react';
 import { AllCars } from './data';
 
-export default function CarsComponent() {
+export default function CarsComponent () {
     return (
-        <div className="showroom-container"></div>
+        <div className="showroom-container">
+            <div className='car-container'>
+                <h3 className="car-title"></h3>
+                <p className="car-price"></p>
+                <div>
+                    <img alt="car" className="product-img" key={index} src={"./carShowroom/" + car.img} />
+                </div>
+            </div>
+        </div>
     );
 }
