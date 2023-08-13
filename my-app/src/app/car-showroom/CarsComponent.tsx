a51d730bf51b8ff5e4e6524d7b0063ef2f228e47
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-13 15:01:53 +0100

    Added the html for the filter component

--- my-app/src/app/car-showroom/CarsComponent.tsx
+++ my-app/src/app/car-showroom/CarsComponent.tsx
@@ -6,15 +6,18 @@ import { AllCars } from './data';
 export default function CarsComponent() {
     {AllCars.map((car, index) => {
         return (
-            <div className="showroom-container" key={index}>
-                <div className='car-container'>
-                    <h3 className="car-title">{car.make} <span>{car.model}</span></h3>
-                    <p className="car-price">{car.price}</p>
-                    <div>
-                        <img alt="car" className="product-img" key={index} src={"./carShowroom/" + car.img} />
+            <section className="box" key={index}>
+                <carShowroomFilter />
+                <div className="showroom-container">
+                    <div className='car-container'>
+                        <h3 className="car-title">{car.make} <span>{car.model}</span></h3>
+                        <p className="car-price">{car.price}</p>
+                        <div>
+                            <img alt="car" className="product-img" key={index} src={"./carShowroom/" + car.img} />
+                        </div>
                     </div>
                 </div>
-            </div>
+            </section>
             );
         });
     }
