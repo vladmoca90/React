06f99c70f212d1793b3b154ea709ab5f760e1a7c
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-15 21:04:41 +0100

    Started to create the files for the tube status project

---
+++ my-app/src/app/tube-status/page.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
+import React from 'react';
+import TubeComponent from './TubeComponent';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './wine-sell.css';
+
+export default function WinesPage() {
+    return (<TubeComponent />);
+}
