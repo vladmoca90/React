392fdd29f138444b394d1a48e80f0aaba6713213
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-16 21:15:08 +0100

    Renamed the .css. file as tube-status.css

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import TubeComponent from './TubeComponent';
 
 import 'bootstrap/dist/css/bootstrap.css';
-import './wine-sell.css';
+import './tube-status.css';
 
 export default function TubePage() {
     return (<TubeComponent />);
