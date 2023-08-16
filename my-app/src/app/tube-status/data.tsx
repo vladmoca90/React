9081d2688d75d53389c111beb2a84fa65ca86795
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-16 20:44:06 +0100

    Added the color property to the main object

--- my-app/src/app/tube-status/data.tsx
+++ my-app/src/app/tube-status/data.tsx
@@ -1,4 +1,5 @@
 export interface tubeStatus {
+    color: statusColors,
     name: string,
     status: string,
 }
@@ -11,62 +12,77 @@ export enum statusColors {
 
 export const tubeLines: tubeStatus[] = [
     {
+        color: statusColors.Green,
         name: 'Bakerloo',
         status: 'Good service',
     },
     {
+        color: statusColors.Green,
         name: 'Central',
         status: 'Good service',
     },
     {
+        color: statusColors.Green,
         name: 'Circle',
         status: 'Good service',
     },
     {
+        color: statusColors.Red,
         name: 'District',
         status: 'Partially closed',
     },
     {
+        color: statusColors.Orange,
         name: 'Hammersmith & City',
         status: 'Partially closed',
     },
     {
+        color: statusColors.Green,
         name: 'Jubilee',
         status: 'Good service',
     },
     {
+        color: statusColors.Orange,
         name: 'Metropolitan',
         status: 'Partially closed',
     },
     {
+        color: statusColors.Green,
         name: 'Northern',
         status: 'Good service',
     },
     {
+        color: statusColors.Green,
         name: 'Picadilly',
         status: 'Good service',
     },
     {
+        color: statusColors.Green,
         name: 'Victoria',
         status: 'Good service',
     },
     {
+        color: statusColors.Red,
         name: 'Waterloo & City',
         status: 'Closed',
     },
     {
+        color: statusColors.Green,
         name: 'London Overground',
         status: 'Good service',
     },
     {
+        color: statusColors.Green,
         name: 'Tfl Rail',
         status: 'Good service',
     },
     {
+        color: statusColors.Green,
         name: 'DLR',
         status: 'Good service',
     },
     {
+        color: statusColors.Red,
         name: 'Croydon Tram',
         status: 'Closed',
     },
