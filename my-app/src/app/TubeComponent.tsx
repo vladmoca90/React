a41a3dffdc1d67630bfddb13525cb4c679e261ea
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-17 20:13:53 +0100

    Added style={{color: line.color}} to the .tube-status

--- my-app/src/app/TubeComponent.tsx
+++ my-app/src/app/TubeComponent.tsx
@@ -27,7 +27,7 @@ export default function TubeComponent() {
                                 return (
                                     <tr key={index}>
                                         <td className="tube-lines">{line.name}</td>
-                                        <td className="tube-status" style={{color: "red"}}>{line.status}</td>
+                                        <td className="tube-status" style={{color: line.color}}>{line.status}</td>
                                     </tr>
                                 );
                             })
