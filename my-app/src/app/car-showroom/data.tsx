ade8a8f6e36ea396f2dc19d693bf52472c4322cb
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-20 00:39:18 +0100

    Added the color property to the object

--- my-app/src/app/car-showroom/data.tsx
+++ my-app/src/app/car-showroom/data.tsx
@@ -1,4 +1,5 @@
 export interface Car {
+    color: CarsColor,
     img: string,
     make: string;
     model: string;
@@ -15,6 +16,7 @@ export enum CarsColor {
 
 export const Cars: Car[] = [
     {
+        color: CarsColor.Black,
         img: 'audi.jpg',
         make: 'Audi',
         model: 'TT 2.0T FSI 2dr',
@@ -22,6 +24,7 @@ export const Cars: Car[] = [
         year: 2007,
     },
     {
+        color: CarsColor.White,
         img: 'land-rover.jpg',
         make: 'Land Rover',
         model: 'Discovery 2.7 Td V6 HSE 5dr Auto',
@@ -29,6 +32,7 @@ export const Cars: Car[] = [
         year: 2009,
     },
     {
+        color: CarsColor.Red,
         img: 'mercedes-benz.jpg',
         make: 'Mercedes-Benz',
         model: 'S Class S500 4dr Auto',
@@ -36,6 +40,7 @@ export const Cars: Car[] = [
         year: 2006,
     },
     {
+        color: CarsColor.Black,
         img: 'nissan.jpg',
         make: 'Nissan',
         model: 'Fairlady Turbo 3.5 V6 300 GT 3d',
@@ -43,6 +48,7 @@ export const Cars: Car[] = [
         year: 2007,
     },
     {
+        color: CarsColor.Red,
         img: 'skoda.jpg',
         make: 'Skoda',
         model: 'Fabia 1.2 TSI SE 5dr',
@@ -50,6 +56,7 @@ export const Cars: Car[] = [
         year: 2011,
     },
     {
+        color: CarsColor.Blue,
         img: 'vauxhall.jpg',
         make: 'Vauxhall',
         model: 'Mokka 1.6 CDTi Limited Edition 5dr',
