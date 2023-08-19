9daa57594308966c8010c018daf9d96cec715c12
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-20 00:37:49 +0100

    Deleted the useEffect import

--- my-app/src/app/car-showroom/CarsComponent.tsx
+++ my-app/src/app/car-showroom/CarsComponent.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { CarsFilterComponent } from './CarsFilterComponent';
 import { Cars } from './data';
 
