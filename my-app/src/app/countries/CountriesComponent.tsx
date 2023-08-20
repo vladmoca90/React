570f333fc15012ef40aa7774ad6f2d5fa1430b68
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-20 01:35:00 +0100

    Created the basic files for the Countries project

---
+++ my-app/src/app/countries/CountriesComponent.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
+import React, { useState } from 'react';
+import { countries } from './data';
+
+export default function CountriesComponent() {
+
+}
---
+++ my-app/src/app/countries/page.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
+import React from 'react';
+import CountriesComponent from './CountriesComponent';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './countries.css';
+
+export default function TubePage() {
+    return (<CountriesComponent />);
+}
