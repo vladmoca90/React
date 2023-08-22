79522787389fc33692a53a404abd67351fc82c8e
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-22 20:36:17 +0100

    Fixed the mapping function

--- my-app/src/app/CarsComponent.tsx
+++ my-app/src/app/CarsComponent.tsx
@@ -7,13 +7,12 @@ import { Cars } from './data';
 export default function CarsComponent() {
     const [allCars] = useState(Cars);
 
-    {
-        allCars.map((car, index) => {
-            return (
-                <section className="box" key={index}>
-                    
-                    <div className="showroom-container">
-                        <div className='car-container'>
+    <section className="box">
+        <div className="showroom-container">
+            {
+                allCars.map((car, index) => {
+                    return (
+                        <div className='car-container' key={index}>
                             <h3 className="car-title">{car.make} <span>{car.model}</span></h3>
                             <p className="car-price">{car.price}</p>
                             <div>
@@ -26,9 +25,9 @@ export default function CarsComponent() {
                                 <a href="">Full details</a>
                             </div>
                         </div>
-                    </div>
-                </section>
-            );
-        });
-    }
+                    );
+                })
+            };
+        </div>
+    </section>
 }
