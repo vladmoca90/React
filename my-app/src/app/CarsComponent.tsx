c1589e670b542ae754f12232c4bf394a67ffd43f
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-22 22:06:41 +0100

    Forgot to add a return statement

--- my-app/src/app/CarsComponent.tsx
+++ my-app/src/app/CarsComponent.tsx
@@ -7,28 +7,29 @@ import { allCars, Car, CarsColor } from './data';
 export default function CarsComponent() {
     const [cars] = useState(allCars);
 
-    <section className="box">
-        <div>Test</div>
-        <div className="showroom-container">
-            {
-                cars.map((car, index) => {
-                    return (
-                        <div className='car-container' key={index}>
-                            <h3 className="car-title">{car.make} <span>{car.model}</span></h3>
-                            <p className="car-price">&pound; {car.price}</p>
-                            <div>
-                                <img alt="car" className="car-img" key={index} src={"./carShowroom/" + car.img} />
+    return (
+        <section className="box">
+            <div className="showroom-container">
+                {
+                    cars.map((car, index) => {
+                        return (
+                            <div className='car-container' key={index}>
+                                <h3 className="car-title">{car.make} <span>{car.model}</span></h3>
+                                <p className="car-price">&pound; {car.price}</p>
+                                <div>
+                                    <img alt="car" className="car-img" key={index} src={"./carShowroom/" + car.img} />
+                                </div>
+                                <div className="showroom-buttons">
+                                    <a href="">Enquiry</a>
+                                    <a href="">Share</a>
+                                    <a href="">Brochure</a>
+                                    <a href="">Full details</a>
+                                </div>
                             </div>
-                            <div className="showroom-buttons">
-                                <a href="">Enquiry</a>
-                                <a href="">Share</a>
-                                <a href="">Brochure</a>
-                                <a href="">Full details</a>
-                            </div>
-                        </div>
-                    );
-                })
-            };
-        </div>
-    </section>
+                        );
+                    })
+                };
+            </div>
+        </section>
+    );
 }
