f978bdbdfc8260753900d92792ed4e4cfedddf06
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-29 01:23:32 +0100

    Added a dropdown with prices

--- my-app/src/app/CarsComponent.tsx
+++ my-app/src/app/CarsComponent.tsx
@@ -6,27 +6,32 @@ import { allCars, Car, CarsColor } from './data';
 
 export default function CarsComponent() {
     const [cars] = useState(allCars);
- 
+
     return (
         <section className="box">
             <div className="showroom-search">
-                <select  id="carMake" title="carMake" >
-                {
-                    cars.map((car, index) => {
-                        return (
-                            <option key={index}>{car.make}</option>
-                        );
-                    })
-                }
+                <select id="carMake" title="carMake" >
+                    {
+                        cars.map((car, index) => {
+                            return (
+                                <option value={car.make} key={index}>{car.make}</option>
+                            );
+                        })
+                    }
                 </select>
-                <select  id="carModel" title="carModel" >
-                {
-                    cars.map((car, index) => {
-                        return (
-                            <option key={index}>{car.model}</option>
-                        );
-                    })
-                }
+                <select id="carModel" title="carModel" >
+                    {
+                        cars.map((car, index) => {
+                            return (
+                                <option value={car.model} key={index}>{car.model}</option>
+                            );
+                        })
+                    }
+                </select>
+                <select id="carPrice" title="carPrice" >
+                    <option>0 - £4999</option>
+                    <option>£5000 - £9999</option>
+                    <option>£10000 - £14999</option>
                 </select>
             </div>
             <div className="showroom-container">
@@ -37,7 +42,7 @@ export default function CarsComponent() {
                                 <div className="car-header">
                                     <h3 className="car-title">{car.make} <span>{car.model}</span></h3>
                                     <p className="car-price">&pound; {car.price}
-                                        <span className="car-monthly-price">from &pound;{(car.price/12).toFixed(0)}/monthly</span>
+                                        <span className="car-monthly-price">from &pound;{(car.price / 12).toFixed(0)}/monthly</span>
                                     </p>
                                 </div>
                                 <div>
