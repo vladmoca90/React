d13bbf39b43c60a26feef5893169bb228d47a58f
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-31 20:25:56 +0100

    Deleted the enum CarsColor { }

--- my-app/src/app/CarsFilterComponent.tsx
+++ my-app/src/app/CarsFilterComponent.tsx
@@ -1,11 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 import React, { useCallback, useState } from 'react';
-import { CarsColor } from './data';
-
-export type CarsFilterProps = {
-    categories: CarsColor;
-}
 
 export const CarsFilterComponent = (props: CarsFilterProps) => {
     
--- my-app/src/app/data.tsx
+++ my-app/src/app/data.tsx
@@ -1,5 +1,4 @@
 export interface Car {
-    color: CarsColor,
     img: string,
     make: string;
     model: string;
@@ -7,16 +6,8 @@ export interface Car {
     year: number;
 }
 
-export enum CarsColor {
-    Black = 'Black',
-    Blue = 'Blue',
-    Red = 'Red',
-    White = 'White',
-}
-
 export const allCars: Car[] = [
     {
-        color: CarsColor.Black,
         img: 'audi.jpg',
         make: 'Audi',
         model: 'TT 2.0T FSI 2dr',
@@ -24,7 +15,6 @@ export const allCars: Car[] = [
         year: 2007,
     },
     {
-        color: CarsColor.White,
         img: 'land-rover.jpg',
         make: 'Land Rover',
         model: 'Discovery 2.7 Td V6 HSE 5dr Auto',
@@ -32,7 +22,6 @@ export const allCars: Car[] = [
         year: 2009,
     },
     {
-        color: CarsColor.Red,
         img: 'mercedes-benz.jpg',
         make: 'Mercedes-Benz',
         model: 'S Class S500 4dr Auto',
@@ -40,7 +29,7 @@ export const allCars: Car[] = [
         year: 2006,
     },
     {
-        color: CarsColor.Black,
+        
         img: 'nissan.jpg',
         make: 'Nissan',
         model: 'Fairlady Turbo 3.5 V6 300 GT 3d',
@@ -48,7 +37,6 @@ export const allCars: Car[] = [
         year: 2007,
     },
     {
-        color: CarsColor.Red,
         img: 'skoda.jpg',
         make: 'Skoda',
         model: 'Fabia 1.2 TSI SE 5dr',
@@ -56,7 +44,6 @@ export const allCars: Car[] = [
         year: 2011,
     },
     {
-        color: CarsColor.Blue,
         img: 'vauxhall.jpg',
         make: 'Vauxhall',
         model: 'Mokka 1.6 CDTi Limited Edition 5dr',
