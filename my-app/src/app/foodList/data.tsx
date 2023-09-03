ff93c00aab045dc9f4c7d1983d3db0d26206ecc5
Vlad Mocanu <vladmoca90@gmail.com>
2023-09-03 20:28:10 +0100

    Created the first sample of the data object

---
+++ my-app/src/app/foodList/data.tsx
@@ -0,0 +1,15 @@
+interface Food {
+    name: string,
+    price: number,
+    quantity: number,
+    unit: string,
+}
+
+export const allFoods: Food[] = [
+    {
+        name: 'Tomatoes',
+        price: 2.99,
+        quantity: 1,
+        unit: 'kg',
+    }
+];
