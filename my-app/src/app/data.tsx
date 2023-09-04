d4a68090784667bd44e229266354842889e8fcb2
Vlad Mocanu <vladmoca90@gmail.com>
2023-09-04 22:41:26 +0100

    Image issue is fixed now. All end with .PNG

--- my-app/src/app/data.tsx
+++ my-app/src/app/data.tsx
@@ -8,56 +8,56 @@ export interface Food {
 
 export const allFoods: Food[] = [
     {
-        img: 'cheese.png',
+        img: 'cheese.PNG',
         name: 'Cheese',
         price: 7.99, //per kg
         quantity: .4,
         unit: 'kg',
     },
     {
-        img: 'chocolate.png',
+        img: 'chocolate.PNG',
         name: 'Chocolate',
         price: 1.89, //per 100g
         quantity: 225,
         unit: 'g',
     },
     {
-        img: 'cucumber.png',
+        img: 'cucumber.PNG',
         name: 'Cucumber',
         price: 3.49, //per piece
         quantity: 1.5,
         unit: 'kg',
     },
     {
-        img: 'milk.png',
+        img: 'milk.PNG',
         name: 'Milk',
         price: .99, //per pint
         quantity: 3.5,
         unit: 'pint',
     },
     {
-        img: 'onion.png',
+        img: 'onion.PNG',
         name: 'Onion',
         price: .79, //each
         quantity: 4,
         unit: 'piece',
     },
     {
-        img: 'pork-meat.png',
+        img: 'pork-meat.PNG',
         name: 'Meat (pork)',
         price: 4.99, //per kg
         quantity: 1.5,
         unit: 'kg',
     },
     {
-        img: 'tomato.png',
+        img: 'tomato.PNG',
         name: 'Tomatoes',
         price: 3.99, //per kg
         quantity: 1.15,
         unit: 'kg',
     },
     {
-        img: 'whiskey.png',
+        img: 'whiskey.PNG',
         name: 'Whiskey',
         price: 19.99, //per bottle
         quantity: 2,
