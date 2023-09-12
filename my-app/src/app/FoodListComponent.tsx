402648122d2c27ecb0e0173b9a37aea8a880559a
Vlad Mocanu <vladmoca90@gmail.com>
2023-09-13 01:07:35 +0300

    Created the const totalPriceForItem which will calculate the real price for each time according to the bought quantity

--- my-app/src/app/FoodListComponent.tsx
+++ my-app/src/app/FoodListComponent.tsx
@@ -13,13 +13,16 @@ import { allFoods } from './data';
 export default function FoodListComponent() {
     const [foods] = useState(allFoods);
 
+    const totalPriceForItem = useCallback(() => {
+
+    }, []);
+
     const totalPrice = useCallback(() => {
         let sum = 0;
 
-        for (let i = 0; i < foods.length; i++) {
-            sum += foods[i].price;
-            
-        }
+        for (let i = 0; i < foods.length; i++) {;
+            sum += (foods[i].price);
+        };
 
         return sum.toFixed(2);
     }, [foods]);
@@ -48,7 +51,7 @@ export default function FoodListComponent() {
                                         <td className="food-name">{food.name}</td>
                                         <td className="food-price">{food.price}</td>
                                         <td className="food-quantity">{food.quantity}</td>
-                                        <td className="food-total-price"></td>
+                                        <td className="food-total-price">{totalPriceForItem()}</td>
                                     </tr>
                                 );
                             })
