b029db690e599d972b146e10882262b3af1edb30
Vlad Mocanu <vladmoca90@gmail.com>
2023-09-13 22:33:04 +0300

    Deleted the FoodCalculusComponent again

--- my-app/src/app/FoodListComponent.tsx
+++ my-app/src/app/FoodListComponent.tsx
@@ -1,11 +1,30 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import Image from 'next/image'
 import { allFoods } from './data';
-import { FoodCalculusComponent } from './FoodCalculusComponent';
 
 export default function FoodListComponent() {
     const [foods] = useState(allFoods);
+
+    const calculateTotalPrice = useCallback(() => {
+        let sum = 0;
+    
+        for (let i = 0; i < foods.length; i++) {
+            sum += (foods[i].price) * (foods[i].quantity);
+        };
+    
+        return sum.toFixed(2);
+    }, [foods]);
+    
+    const calculateAveragePrice = useCallback(() => {
+        let average = 0;
+    
+        for (let i = 0; i < foods.length; i++) {
+            average += (foods[i].price * foods[i].quantity) / (foods.length);
+        }
+    
+        return average;
+    }, [foods]);
 
     return (
         <main className="main">
@@ -39,7 +58,26 @@ export default function FoodListComponent() {
                     </tbody>
                 </table>
             </div>
-            <FoodCalculusComponent foods={[]} />
+            <div className="container-calculus" >
+                <table className="table-main table table-container-calculus">
+                    <thead className="table-header table-header-calculus">
+                        <tr>
+                            <th scope="col">Total amount to pay</th>
+                            <th scope="col">Average price of each item</th>
+                            <th scope="col">Cheapest item</th>
+                            <th scope="col">Most expensive item</th>
+                        </tr>
+                    </thead>
+                    <tbody className="table-content table-content-calculus">
+                        <tr>
+                            <td>&pound;{calculateTotalPrice().toFixed(3)}</td>
+                            <td>&pound;{calculateAveragePrice().toFixed(3)}</td>
+                            <td>&pound;</td>
+                            <td>&pound;</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </main>
     );
 }
@@ -51,7 +89,6 @@ const calculateTotalPrice = useCallback(() => {
     let sum = 0;
 
     for (let i = 0; i < foods.length; i++) {
-        ;
         sum += (foods[i].price) * (foods[i].quantity);
     };
 
