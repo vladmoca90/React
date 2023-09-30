ded7a79335fbabe33afcc80286565b8d99a56628
Vlad Mocanu <vladmoca90@gmail.com>
2023-10-01 00:16:26 +0100

    Created the files for the ToDoList project

---
+++ my-app/src/app/to-do-list/ToDoListComponent.tsx
@@ -0,0 +1,6 @@
+"use client";
+import { useCallback, useState } from 'react';
+
+export default function ToDoListComponent() {
+    
+}
---
+++ my-app/src/app/to-do-list/page.tsx
@@ -0,0 +1,8 @@
+import ToDoListComponent from './ToDoListComponent';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './to-do-list.css';
+
+export default function ToDoListPage() {
+    return (<ToDoListComponent />);
+}
