1fc999a4b229ac080bd6ff00197b2c4070791e87
Vlad Mocanu <vladmoca90@gmail.com>
2023-09-30 23:49:40 +0100

    Created the CarsFilterComponent file .tsx

--- my-app/src/app/CarsComponent.tsx
+++ my-app/src/app/CarsComponent.tsx
@@ -2,40 +2,14 @@
 import Image from 'next/image';
 import { useState } from 'react';
 import { allCars } from './data';
+import { CarsFilterComponent } from './CarsFilterComponent';
 
 export default function CarsComponent() {
     const [cars] = useState(allCars);
 
     return (
         <section className="box">
-            <div className="showroom-search">
-                <select id="carMake" title="carMake">
-                    <option value="make">-- Make --</option>
-                    {
-                        cars.map((car, index) => {
-                            return (
-                                <option value={car.make} key={index}>{car.make}</option>
-                            );
-                        })
-                    }
-                </select>
-                <select id="carModel" title="carModel">
-                    <option value="model">-- Model --</option>
-                    {
-                        cars.map((car, index) => {
-                            return (
-                                <option value={car.model} key={index}>{car.model}</option>
-                            );
-                        })
-                    }
-                </select>
-                <select id="carPrice" title="carPrice">
-                    <option value="price">-- Price --</option>
-                    <option value="low">0 - &pound;4999</option>
-                    <option value="medium">&pound;5000 - &pound;9999</option>
-                    <option value="high">&pound;10000 - &pound;14999</option>
-                </select>
-            </div>
+            <CarsFilterComponent />
             <div className="showroom-container">
                 {
                     cars.map((car, index) => {
---
+++ my-app/src/app/CarsFilterComponent.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { useState, useCallback } from 'react';
+import { allCars } from './data';
+
+export const CarsFilterComponent = () => {
+    const [cars] = useState(allCars);
+
+    return (
+        <div className="showroom-search">
+            <select id="carMake" title="carMake">
+                <option value="make">-- Make --</option>
+                {
+                    cars.map((car, index) => {
+                        return (
+                            <option value={car.make} key={index}>{car.make}</option>
+                        );
+                    })
+                }
+            </select>
+            <select id="carModel" title="carModel">
+                <option value="model">-- Model --</option>
+                {
+                    cars.map((car, index) => {
+                        return (
+                            <option value={car.model} key={index}>{car.model}</option>
+                        );
+                    })
+                }
+            </select>
+            <select id="carPrice" title="carPrice">
+                <option value="price">-- Price --</option>
+                <option value="low">0 - &pound;4999</option>
+                <option value="medium">&pound;5000 - &pound;9999</option>
+                <option value="high">&pound;10000 - &pound;14999</option>
+            </select>
+        </div>
+    );
+}
