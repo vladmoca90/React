266694c00bbcb081b15e88854a1d9ab7be621332
Vlad Mocanu <vladmoca90@gmail.com>
2023-10-01 00:27:29 +0100

    Added an HTML for the ToDoList

--- my-app/src/app/to-do-list/ToDoListComponent.tsx
+++ my-app/src/app/to-do-list/ToDoListComponent.tsx
@@ -2,5 +2,20 @@
 import { useCallback, useState } from 'react';
 
 export default function ToDoListComponent() {
-    
+    <section>
+        <div className="container">
+            <h3 className="title">To do list</h3>
+            <div className="main-container">
+                <div className="type-field">
+                    <input aria-label="text" aria-label="text" type="text" id="field" className="form-control" placeholder="Write here..." />
+                    <a className="add-button">+ Add</a>
+                </div>
+                <div className="list-container">
+                    <ul className="list-main">
+                        <li className="list-item"></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
 }
