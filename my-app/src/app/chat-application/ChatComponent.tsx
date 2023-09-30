4c4afcd2727a00ad7405249a5d2b508c02b2b2f7
Vlad Mocanu <vladmoca90@gmail.com>
2023-10-01 00:39:10 +0100

    Created the files for the ChatApplication project

---
+++ my-app/src/app/chat-application/ChatComponent.tsx
@@ -0,0 +1,6 @@
+"use client";
+import { useCallback } from 'react';
+
+export default function ChatComponent() {
+    return <section></section>;
+}
---
+++ my-app/src/app/chat-application/page.tsx
@@ -0,0 +1,8 @@
+import ChatComponent from './ChatComponent';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './chat-application.css';
+
+export default function ToDoListPage() {
+    return (<ChatComponent />);
+}
