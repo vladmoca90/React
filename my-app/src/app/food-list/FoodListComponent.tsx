2a126923ada624dce846a075a54d51522a3a81b1
Vlad Mocanu <vladmoca90@gmail.com>
2023-10-01 00:16:10 +0100

    Added the import React only in the index.js

--- my-app/src/app/food-list/FoodListComponent.tsx
+++ my-app/src/app/food-list/FoodListComponent.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useCallback, useState } from 'react';
+import { useCallback, useState } from 'react';
 import Image from 'next/image'
 import { allFoods } from './data';
 
