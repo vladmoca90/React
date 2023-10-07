751359fc1f73dbb0f38825f84fe60f7f1bd9a863
Vlad Mocanu <vladmoca90@gmail.com>
2023-10-07 21:17:21 +0100

    Deleted a useless space

--- my-app/src/app/CarsComponent.tsx
+++ my-app/src/app/CarsComponent.tsx
@@ -16,7 +16,7 @@ export default function CarsComponent() {
                         return (
                             <div className="car-container" key={index}>
                                 <div className="car-header">
-                                    <h3 className="car-title">{car.make} <span>{car.model}</span></h3>
+                                    <h3 className="car-title">{car.make}<span>{car.model}</span></h3>
                                     <p className="car-price">&pound;{car.price}
                                         <span className="car-monthly-price">from &pound;{(car.price / 12).toFixed(0)}/monthly</span>
                                     </p>
