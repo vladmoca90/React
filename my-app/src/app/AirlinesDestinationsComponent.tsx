d1fadf95656fd7e38a0fc384e039df63ccbe544d
Vlad Mocanu <vladmoca90@gmail.com>
2023-10-13 21:07:15 +0100

    Deleted the constant from the API fetch

--- my-app/src/app/AirlinesDestinationsComponent.tsx
+++ my-app/src/app/AirlinesDestinationsComponent.tsx
@@ -8,47 +8,45 @@ export default function AirlinesDestinationsComponent() {
 
   let airlinesDestUrl = 'http://localhost:3000/airlines-destinations';
 
-  const airlinesDestValues = useEffect(() => {
+  useEffect(() => {
     fetch(airlinesDestUrl)
       .then(data => {
         console.log(data);
       })
       .then((result) => {
-        
+
       },
         (error) => {
-          
-        });
-  }, []);
 
-    return (
-      <main className="main">
-        <div className="container-table">
-          <table className="table-main table">
-            <thead className="table-header">
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">Name</th>
-                <th scope="col">Destinations</th>
-              </tr>
-            </thead>
-            <tbody className="table-content">
-              {
-                flights.map((flight, index) => {
-                  return (
-                    <tr key={index}>
-                      <td className="airline-number">{index + 1}</td>
-                      <td className="airline-name">{flight.airline}</td>
-                      <td className="airline-destinations-list">{flight.destinations.toString().replace(/,/g, ', ')}</td>
-                    </tr>
-                  );
-                })
-              }
-            </tbody>
-          </table>
-          <TotalDestinations flights={flights} />
-        </div>
-      </main>
-    );
-  }
+        });
+  }, [airlinesDestUrl]);
+  return (
+    <main className="main">
+      <div className="container-table">
+        <table className="table-main table">
+          <thead className="table-header">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">Destinations</th>
+            </tr>
+          </thead>
+          <tbody className="table-content">
+            {
+              flights.map((flight, index) => {
+                return (
+                  <tr key={index}>
+                    <td className="airline-number">{index + 1}</td>
+                    <td className="airline-name">{flight.airline}</td>
+                    <td className="airline-destinations-list">{flight.destinations.toString().replace(/,/g, ', ')}</td>
+                  </tr>
+                );
+              })
+            }
+          </tbody>
+        </table>
+        <TotalDestinations flights={flights} />
+      </div>
+    </main>
+  );
 }
