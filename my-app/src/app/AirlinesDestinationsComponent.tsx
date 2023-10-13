29e775d4589ba89032f3853254e802ba608b3e48
Vlad Mocanu <vladmoca90@gmail.com>
2023-10-13 20:59:47 +0100

    Deleted the expressions that were not working

--- my-app/src/app/AirlinesDestinationsComponent.tsx
+++ my-app/src/app/AirlinesDestinationsComponent.tsx
@@ -14,40 +14,41 @@ export default function AirlinesDestinationsComponent() {
         console.log(data);
       })
       .then((result) => {
-
+        
       },
         (error) => {
-          console.log(error);
+          
         });
-  }, [airlinesDestUrl]);
+  }, []);
 
-  return (
-    <main className="main">
-      <div className="container-table">
-        <table className="table-main table">
-          <thead className="table-header">
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Name</th>
-              <th scope="col">Destinations</th>
-            </tr>
-          </thead>
-          <tbody className="table-content">
-            {
-              flights.map((flight, index) => {
-                return (
-                  <tr key={index}>
-                    <td className="airline-number">{index + 1}</td>
-                    <td className="airline-name">{flight.airline}</td>
-                    <td className="airline-destinations-list">{flight.destinations.toString().replace(/,/g, ', ')}</td>
-                  </tr>
-                );
-              })
-            }
-          </tbody>
-        </table>
-        <TotalDestinations flights={flights} />
-      </div>
-    </main>
-  );
+    return (
+      <main className="main">
+        <div className="container-table">
+          <table className="table-main table">
+            <thead className="table-header">
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Destinations</th>
+              </tr>
+            </thead>
+            <tbody className="table-content">
+              {
+                flights.map((flight, index) => {
+                  return (
+                    <tr key={index}>
+                      <td className="airline-number">{index + 1}</td>
+                      <td className="airline-name">{flight.airline}</td>
+                      <td className="airline-destinations-list">{flight.destinations.toString().replace(/,/g, ', ')}</td>
+                    </tr>
+                  );
+                })
+              }
+            </tbody>
+          </table>
+          <TotalDestinations flights={flights} />
+        </div>
+      </main>
+    );
+  }
 }
