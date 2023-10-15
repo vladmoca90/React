0fecc3e11a94e3e0b912e8b43c60ce7c982ad3b4
Vlad Mocanu <vladmoca90@gmail.com>
2023-10-15 03:12:19 +0100

    Added a class and styling for the Loading text

--- my-app/src/app/AirlinesDestinationsComponent.tsx
+++ my-app/src/app/AirlinesDestinationsComponent.tsx
@@ -5,11 +5,14 @@ import { TotalDestinations } from './TotalDestinationsComponent';
 
 export type AirlinesDestinationsProps = {
     response: undefined;
-    error: string;
+    error: null;
+    flight: [];
 }
 
 export default function AirlinesDestinationsComponent() {
-    const [flights] = useState(allFlights);
+    const [error, setError] = useState(null);
+    const [isLoaded] = useState(false);
+    const [flights, setFlights] = useState([]);
 
     let airlinesDestinationsUrl = "http://localhost:3000/airlines-destinations";
 
@@ -17,36 +20,42 @@ export default function AirlinesDestinationsComponent() {
         fetch(airlinesDestinationsUrl)
             .then(response => response.json())
             .then(res => console.log(res))
-            
+            .catch(error =>
+                setError(error))
     }, [airlinesDestinationsUrl]);
-
-    return (
-        <main className="main">
-            <div className="container-table">
-                <table className="table-main table">
-                    <thead className="table-header">
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Name</th>
-                            <th scope="col">Destinations</th>
-                        </tr>
-                    </thead>
-                    <tbody className="table-content">
-                        {
-                            flights.map((flight, index) => {
-                                return (
-                                    <tr key={index}>
-                                        <td className="airline-number">{index + 1}</td>
-                                        <td className="airline-name">{flight.airline}</td>
-                                        <td className="airline-destinations-list">{flight.destinations.toString().replace(/,/g, ', ')}</td>
-                                    </tr>
-                                );
-                            })
-                        }
-                    </tbody>
-                </table>
-                <TotalDestinations flights={flights} />
-            </div>
-        </main>
-    );
+    if (error) {
+        return <div>Error: {error}</div>
+    } else if (!isLoaded) {
+        return <div className="loading-flights">Loading...</div>;
+    } else {
+        return (
+            <main className="main">
+                <div className="container-table">
+                    <table className="table-main table">
+                        <thead className="table-header">
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Destinations</th>
+                            </tr>
+                        </thead>
+                        <tbody className="table-content">
+                            {
+                                flights.map((flight, index) => {
+                                    return (
+                                        <tr key={index}>
+                                            <td className="airline-number">{index + 1}</td>
+                                            <td className="airline-name">{flight.airline}</td>
+                                            <td className="airline-destinations-list">{flight.destinations.toString().replace(/,/g, ', ')}</td>
+                                        </tr>
+                                    );
+                                })
+                            }
+                        </tbody>
+                    </table>
+                    <TotalDestinations flights={flights} />
+                </div>
+            </main>
+        );
+    }
 }
