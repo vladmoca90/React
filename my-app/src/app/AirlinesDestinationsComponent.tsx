3c245e1b005d5030ca1cb9c7fad888e47eed23b1
Vlad Mocanu <vladmoca90@gmail.com>
2023-10-15 03:28:04 +0100

    API request works

--- my-app/src/app/AirlinesDestinationsComponent.tsx
+++ my-app/src/app/AirlinesDestinationsComponent.tsx
@@ -1,61 +1,60 @@
 "use client";
 import { useEffect, useState } from 'react';
-import { allFlights } from './airlines-destinations/route';
+// import { allFlights } from './airlines-destinations/route';
 import { TotalDestinations } from './TotalDestinationsComponent';
 
-export type AirlinesDestinationsProps = {
-    response: undefined;
-    error: null;
-    flight: [];
+interface Flight {
+    airline: string;
+    destinations: string[];
 }
 
 export default function AirlinesDestinationsComponent() {
     const [error, setError] = useState(null);
     const [isLoaded] = useState(false);
-    const [flights, setFlights] = useState([]);
+    const [flights, setFlights] = useState<Flight[]>([]);
 
     let airlinesDestinationsUrl = "http://localhost:3000/airlines-destinations";
 
     useEffect(() => {
         fetch(airlinesDestinationsUrl)
-            .then(response => response.json())
-            .then(res => console.log(res))
+            .then((response) => response)
+            .then(e => e.json())
+            .then(e => setFlights(e as Flight[]))
             .catch(error =>
                 setError(error))
     }, [airlinesDestinationsUrl]);
-    if (error) {
-        return <div>Error: {error}</div>
-    } else if (!isLoaded) {
-        return <div className="loading-flights">Loading...</div>;
-    } else {
-        return (
-            <main className="main">
-                <div className="container-table">
-                    <table className="table-main table">
-                        <thead className="table-header">
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Name</th>
-                                <th scope="col">Destinations</th>
-                            </tr>
-                        </thead>
-                        <tbody className="table-content">
-                            {
-                                flights.map((flight, index) => {
-                                    return (
-                                        <tr key={index}>
-                                            <td className="airline-number">{index + 1}</td>
-                                            <td className="airline-name">{flight.airline}</td>
-                                            <td className="airline-destinations-list">{flight.destinations.toString().replace(/,/g, ', ')}</td>
-                                        </tr>
-                                    );
-                                })
-                            }
-                        </tbody>
-                    </table>
-                    <TotalDestinations flights={flights} />
-                </div>
-            </main>
-        );
-    }
+    /* if (error) {
+         return <div>Error: {error}</div>
+     } else if (!isLoaded) {
+         return <div className="loading-flights">Loading...</div>;
+     } else { */
+    return (
+        <main className="main">
+            <div className="container-table">
+                <table className="table-main table">
+                    <thead className="table-header">
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Destinations</th>
+                        </tr>
+                    </thead>
+                    <tbody className="table-content">
+                        {
+                            flights.map((flight, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <td className="airline-number">{index + 1}</td>
+                                        <td className="airline-name">{flight.airline}</td>
+                                        <td className="airline-destinations-list">{flight.destinations.toString().replace(/,/g, ', ')}</td>
+                                    </tr>
+                                );
+                            })
+                        }
+                    </tbody>
+                </table>
+                <TotalDestinations flights={flights} />
+            </div>
+        </main>
+    );
 }
