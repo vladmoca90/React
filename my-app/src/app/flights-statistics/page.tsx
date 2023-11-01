1b6635bfbf4009f30841b68e5c113c4442380971
Vlad Mocanu <vladmoca90@gmail.com>
2023-11-01 21:43:47 +0000

    Created the basis for a future project of flights statistics

---
+++ my-app/src/app/flights-statistics/page.tsx
@@ -0,0 +1,8 @@
+import FlightsStatisticsComponent from './FlightsStatisticsComponent';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './flights-statistics.css';
+
+export default function DestinationsPage() {
+    return (<FlightsStatisticsComponent />);
+}
