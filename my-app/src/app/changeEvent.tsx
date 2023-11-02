085e0121238e383db8c9369a7001f5b0629e40fd
Vlad Mocanu <vladmoca90@gmail.com>
2023-11-02 19:25:09 +0000

    Created a change Event project

---
+++ my-app/src/app/changeEvent.tsx
@@ -0,0 +1,48 @@
+// App.tsx
+// Kindacode.com
+import React, { useState } from "react";
+
+const App = () => {
+    const [selectedOption, setSelectedOption] = useState<String>();
+
+    // This function is triggered when the select changes
+    const selectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        const value = event.target.value;
+        setSelectedOption(value);
+    };
+
+    return (
+        <div style={styles.container}>
+            <select onChange={selectChange} style={styles.select}>
+                <option selected disabled>
+                    Choose one
+                </option>
+                <option value="blue">Blue</option>
+                <option value="red">Red</option>
+                <option value="green">Green</option>
+                <option value="yellow">Yellow</option>
+                <option value="kindacode.com">Kindacode.com</option>
+            </select>
+            {selectedOption && <h2 style={styles.result}>{selectedOption}</h2>}
+        </div>
+    );
+};
+
+export default App;
+
+// Just some styles
+const styles: { [name: string]: React.CSSProperties } = {
+    container: {
+        marginTop: 50,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+    },
+    select: {
+        padding: 5,
+        width: 200,
+    },
+    result: {
+        marginTop: 30,
+    },
+};
