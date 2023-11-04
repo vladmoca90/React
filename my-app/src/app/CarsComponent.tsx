5a2e41f069dfebcdb5e2b8a25765fd7cf93617ff
Vlad Mocanu <vladmoca90@gmail.com>
2023-11-04 23:01:10 +0000

    Formatted the file

--- my-app/src/app/CarsComponent.tsx
+++ my-app/src/app/CarsComponent.tsx
@@ -17,7 +17,7 @@ export default function CarsComponent() {
 
     //A function that removes the duplicates from the array and display each value once in the dropdown
     const removeDuplicatedMakes = useCallback(() => {
-        let carsDictionary: {[make: string[number]]: any} = {};
+        let carsDictionary: { [make: string[number]]: any } = {};
 
         for (let i = 0; i < cars.length; i++) {
             if (carsDictionary[cars[i].make]) {
