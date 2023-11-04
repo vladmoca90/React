dbf598400c22d51000b805446b36f6accdd1175e
Vlad Mocanu <vladmoca90@gmail.com>
2023-11-04 23:47:41 +0000

    Added a link with ideas on how to create a Weather App project

--- my-app/src/app/weather-app/WeatherAppComponent.tsx
+++ my-app/src/app/weather-app/WeatherAppComponent.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { useCallback } from 'react';
 
+//check --> https://www.google.com/search?q=interesting+react+projects+weather+app&sca_esv=579520937&bih=929&biw=1920&rlz=1C1JJTC_en-GBGB1066GB1066&hl=en-GB&sxsrf=AM9HkKnIIs9drusloMh9cgc4N7eMYeRWlQ%3A1699141510140&ei=htdGZY6ZCIO2hbIP1KGRoAg&ved=0ahUKEwiO9Imxw6uCAxUDW0EAHdRQBIQQ4dUDCBA&uact=5&oq=interesting+react+projects+weather+app&gs_lp=Egxnd3Mtd2l6LXNlcnAiJmludGVyZXN0aW5nIHJlYWN0IHByb2plY3RzIHdlYXRoZXIgYXBwMgUQIRigAUj8FFDpBljBE3ABeAGQAQCYAcEBoAGHC6oBAzMuObgBA8gBAPgBAcICChAAGEcY1gQYsAPCAgYQABgWGB7CAggQABiKBRiGA8ICBxAhGKABGArCAggQIRgWGB4YHeIDBBgAIEGIBgGQBgg&sclient=gws-wiz-serp 
+
 export default function WeatherAppComponent() {
     return <section></section>;
 }
