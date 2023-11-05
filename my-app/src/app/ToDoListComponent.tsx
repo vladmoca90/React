6b9796f531bcfb292dde788b9daef58bbf33acbd
Vlad Mocanu <vladmoca90@gmail.com>
2023-11-05 20:09:10 +0000

    Formatted the file

--- my-app/src/app/ToDoListComponent.tsx
+++ my-app/src/app/ToDoListComponent.tsx
@@ -2,21 +2,22 @@
 import { useCallback } from 'react';
 
 export default function ToDoListComponent() {
-    return (<section>
-        <div className="container">
-            <h3 className="title">To do list</h3>
-            <div className="main-container">
-                <div className="type-field">
-                    <input aria-label="text" type="text" id="field" className="form-control" placeholder="Write here..." />
-                    <a className="add-button">+ Add</a>
-                </div>
-                <div className="list-container">
-                    <ul className="list-main">
-                        <li className="list-item"></li>
-                    </ul>
+    return (
+        <section>
+            <div className="container">
+                <h3 className="title">To do list</h3>
+                <div className="main-container">
+                    <div className="type-field">
+                        <input aria-label="text" type="text" id="field" className="form-control" placeholder="Write here..." />
+                        <a className="add-button">+ Add</a>
+                    </div>
+                    <div className="list-container">
+                        <ul className="list-main">
+                            <li className="list-item"></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
     );
 }
