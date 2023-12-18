e1e2eac3c8e0579ec4e0e30b34d27e1f3a271127
Vlad Mocanu <vladmoca90@gmail.com>
2023-12-18 07:41:08 +0000

    Deleted the to do list

--- my-app/src/app/to-do-list/page.tsx
+++
@@ -1,8 +0,0 @@
-import ToDoListComponent from './ToDoListComponent';
-
-import 'bootstrap/dist/css/bootstrap.css';
-import './to-do-list.css';
-
-export default function ToDoListPage() {
-    return (<ToDoListComponent />);
-}
