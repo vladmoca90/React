c777fe5eef50096f3d868c0ed54441e5764fee05
Vlad Mocanu <vladmoca90@gmail.com>
2023-12-19 22:37:57 +0000

    Moved the Cars project in the correct folders

--- my-app/src/app/APIs-old/food-list/route.ts
+++ my-app/src/app/APIs-old/food-list/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { allFoods, Food } from './foods';
+import { allFoods, Food } from './data';
 
 export async function GET() {
     return NextResponse.json(allFoods);
